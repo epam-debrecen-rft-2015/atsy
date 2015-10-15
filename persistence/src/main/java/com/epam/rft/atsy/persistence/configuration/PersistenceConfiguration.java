0d28c2d869e6ea8b82eaf84f47cfd662702e3327
Antal Kiss <v-ankiss@expedia.com>
2015-10-15 14:50:59 +0200

    JPATransaction manager

--- persistence/src/main/java/com/epam/rft/atsy/persistence/configuration/PersistenceConfiguration.java
+++ persistence/src/main/java/com/epam/rft/atsy/persistence/configuration/PersistenceConfiguration.java
@@ -1,20 +1,22 @@
 package com.epam.rft.atsy.persistence.configuration;
 
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
 import org.flywaydb.core.Flyway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
-import java.util.Properties;
 
 /**
  * Created by Ikantik.
@@ -31,19 +33,18 @@ public class PersistenceConfiguration {
         flyway.setBaselineOnMigrate(true);
         flyway.setDataSource(dataSource());
         return flyway;
-
     }
 
     @Bean
     @DependsOn("flyway")
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+    public EntityManagerFactory entityManagerFactory() {
         LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
         em.setDataSource(dataSource());
         em.setPackagesToScan(new String[]{"com.epam.rft.atsy.persistence"});
         JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         em.setJpaVendorAdapter(vendorAdapter);
         em.setJpaProperties(additionalProperties());
-        return em;
+        return em.getNativeEntityManagerFactory();
     }
 
     @Bean
@@ -56,7 +57,7 @@ public class PersistenceConfiguration {
 
     @Bean
     public PlatformTransactionManager txManager() {
-        return new DataSourceTransactionManager(dataSource());
+        return new JpaTransactionManager(entityManagerFactory());
     }
 
     @Bean
@@ -64,7 +65,7 @@ public class PersistenceConfiguration {
         return new PersistenceExceptionTranslationPostProcessor();
     }
 
-    Properties additionalProperties() {
+    private Properties additionalProperties() {
         Properties properties = new Properties();
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
         return properties;
