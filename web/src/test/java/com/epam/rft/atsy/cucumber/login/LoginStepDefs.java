7545d933da456734b4c0ba29b87b530c9325871d
Antal Kiss <ikantik@gmail.com>
2015-10-18 22:07:49 +0200

    run firefox in xvfb

--- web/src/test/java/com/epam/rft/atsy/cucumber/login/LoginStepDefs.java
+++ web/src/test/java/com/epam/rft/atsy/cucumber/login/LoginStepDefs.java
@@ -1,29 +1,23 @@
 package com.epam.rft.atsy.cucumber.login;
 
-import com.epam.rft.atsy.cucumber.util.DriverProvider;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
  * Created by Ikantik.
  */
-
 public class LoginStepDefs {
-
-    private DriverProvider driverProvider;
-
-    public LoginStepDefs(DriverProvider driverProvider) {
-        this.driverProvider = driverProvider;
-    }
+    WebDriver driver = new FirefoxDriver();
 
     @When("^I'm opening (.*)")
     public void openingThePage(String url) {
-        driverProvider.getDriver().get(url);
+        driver.get(url);
     }
 
     @Then("^I'm happy")
     public void i_am_happy() {
-        driverProvider.getDriver().findElement(By.tagName("body"));
+        driver.close();
     }
 }
