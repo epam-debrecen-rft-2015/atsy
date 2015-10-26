cde05692823bb7406911672ab3fe0303e0a29aae
matesszabo <matesszabo@hotmail.com>
2015-10-26 18:28:50 +0100

    Mapper moved to ServiceConfiguration

--- service/src/main/java/com/epam/rft/atsy/service/configuration/ServiceConfiguration.java
+++ service/src/main/java/com/epam/rft/atsy/service/configuration/ServiceConfiguration.java
@@ -1,6 +1,9 @@
 package com.epam.rft.atsy.service.configuration;
 
 import com.epam.rft.atsy.persistence.configuration.PersistenceConfiguration;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -9,5 +12,10 @@ import org.springframework.context.annotation.Import;
  */
 @Configuration
 @Import({PersistenceConfiguration.class})
+@ComponentScan("com.epam.rft.atsy.service")
 public class ServiceConfiguration {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
