14dcaae77961bc13359c6b535284078d02aba35b
Antal Kiss <v-ankiss@expedia.com>
2015-10-29 15:04:15 +0100

    Test fix

--- persistence/src/test/java/com/epam/rft/atsy/persistence/UserDAOImplTest.java
+++ persistence/src/test/java/com/epam/rft/atsy/persistence/UserDAOImplTest.java
@@ -1,37 +1,35 @@
 package com.epam.rft.atsy.persistence;
 
-import com.epam.rft.atsy.persistence.dao.UserDAO;
-import com.epam.rft.atsy.persistence.dao.impl.UserDAOImpl;
-import com.epam.rft.atsy.persistence.entities.UserEntity;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
 
 import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import javax.persistence.NonUniqueResultException;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalToIgnoringCase;
-import static org.hamcrest.Matchers.is;
-import static org.mockito.BDDMockito.given;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.epam.rft.atsy.persistence.dao.impl.UserDAOImpl;
+import com.epam.rft.atsy.persistence.entities.UserEntity;
+
 /**
  * Created by mates on 10/29/2015.
  */
 public class UserDAOImplTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserDAOImplTest.class);
-    private static final String UNAME="username";
-    private static final String PWD="password";
+    private static final String UNAME = "username";
+    private static final String PWD = "password";
 
     @InjectMocks
     UserDAOImpl userDAOImpl;
@@ -55,32 +53,31 @@ public class UserDAOImplTest {
     EntityManager entityManager;
 
     @BeforeMethod
-    public void setUp(){
+    public void setUp() {
         MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    public void loginTest(){
+    public void loginTest() {
 
         //given
-        String username="userName";
-        String password="userPassord";
-        given(userEntity.getUserName()).willReturn("userName");
-        given(userEntity.getUserPassword()).willReturn("userPassword");
+        String username = "userName";
+        String password = "userPassord";
+        given(userEntity.getUserName()).willReturn(username);
+        given(userEntity.getUserPassword()).willReturn(password);
         given(entityManager.getCriteriaBuilder()).willReturn(criteriaBuilder);
         given(criteriaBuilder.createQuery(UserEntity.class)).willReturn(criteriaQuery);
         given(criteriaQuery.from(UserEntity.class)).willReturn(root);
         given(criteriaQuery.where(criteriaBuilder.and(criteriaBuilder.equal(root.get(UNAME), username),
-                criteriaBuilder.equal(root.get(PWD), password)))).willReturn(criteriaQuery);
+            criteriaBuilder.equal(root.get(PWD), password)))).willReturn(criteriaQuery);
         given(entityManager.createQuery(criteriaQuery)).willReturn(userEntityTypedQuery);
+        given(userEntityTypedQuery.getSingleResult()).willReturn(userEntity);
+        UserEntity resultUserEntity = userDAOImpl.login(username, password);
 
-        UserEntity resultUserEntity = userDAOImpl.login(username,password);
-
-        assertThat(resultUserEntity.getUserName(), is(equalToIgnoringCase("userName")));
-        assertThat(resultUserEntity.getUserPassword(), is(equalToIgnoringCase("passWord")));
+        assertThat(resultUserEntity.getUserName(), is(equalToIgnoringCase(username)));
+        assertThat(resultUserEntity.getUserPassword(), is(equalToIgnoringCase(password)));
 
     }
-
 
     //TODO
     //NoResultException - if there is no result
