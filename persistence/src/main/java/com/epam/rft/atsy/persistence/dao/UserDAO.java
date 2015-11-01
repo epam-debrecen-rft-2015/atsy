a36f42ae890503b0e94b33f14f8655cf8f77198a
matesszabo <matesszabo@hotmail.com>
2015-11-01 01:28:11 +0100

    Persistence documentation

--- persistence/src/main/java/com/epam/rft/atsy/persistence/dao/UserDAO.java
+++ persistence/src/main/java/com/epam/rft/atsy/persistence/dao/UserDAO.java
@@ -6,6 +6,12 @@ import com.epam.rft.atsy.persistence.entities.UserEntity;
  * Created by mates on 10/22/2015.
  */
 public interface UserDAO extends GenericDAO<UserEntity, Long>{
-
+    /**
+     * A method for getting a UserEntity from the database.
+     * Returns a UserEntity object represents the matching record of the database.
+     * @param username is the name of the user
+     * @param password is the password of the user
+     * @return UserEntity object represents the matching record of the database
+     */
     UserEntity login(String username, String password);
 }
--- persistence/src/main/java/com/epam/rft/atsy/persistence/dao/impl/UserDAOImpl.java
+++ persistence/src/main/java/com/epam/rft/atsy/persistence/dao/impl/UserDAOImpl.java
@@ -17,9 +17,22 @@ import com.epam.rft.atsy.persistence.entities.UserEntity;
 @Transactional(Transactional.TxType.REQUIRED)
 @Repository
 public class UserDAOImpl extends GenericDAOImpl<UserEntity, Long> implements UserDAO{
+    /**
+     * A constant value for username.
+     */
     private static final String UNAME="userName";
+    /**
+     * A constant value for password.
+     */
     private static final String PWD="userPassword";
 
+    /**
+     * A method for getting a UserEntity from the database.
+     * Returns a UserEntity object represents the matching record of the database.
+     * @param username is the name of the user
+     * @param password is the password of the user
+     * @return UserEntity object represents the matching record of the database
+     */
     public UserEntity login(String username, String password) {
 
         CriteriaBuilder criteriaBuilder=this.entityManager.getCriteriaBuilder();
