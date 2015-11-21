77b24a964c5c2a76cb5f76019c39487c80789881
Antal Kiss <ikantik@gmail.com>
2015-11-21 13:22:50 +0100

    Features by IT

--- web/src/test/java/com/epam/rft/atsy/cucumber/WelcomePageIT.java
+++ web/src/test/java/com/epam/rft/atsy/cucumber/WelcomePageIT.java
@@ -3,10 +3,11 @@ package com.epam.rft.atsy.cucumber;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
+
 /**
  * Created by mates on 2015. 11. 11..
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(format = {"json:target/report.json"}, glue = {"com.epam.rft.atsy.cucumber.welcome", "com.epam.rft.atsy.cucumber.util"})
+@CucumberOptions(format = {"json:target/report.json"}, features = {"com.epam.rft.atsy.cucumber.Welcome.feature"}, glue = {"com.epam.rft.atsy.cucumber.welcome", "com.epam.rft.atsy.cucumber.util"})
 public class WelcomePageIT {
 }
