35edf57bfd4a2590a8af75827cb9b7d820f878ae
Daniel Toth <dponttoth@gmail.com>
2015-11-27 01:40:16 +0100

    CandidateControllerTest

--- web/src/test/java/com/epam/rft/atsy/web/controllers/CandidateControllerTest.java
+++ web/src/test/java/com/epam/rft/atsy/web/controllers/CandidateControllerTest.java
@@ -5,14 +5,18 @@ import com.epam.rft.atsy.persistence.request.SortingRequest;
 import com.epam.rft.atsy.service.CandidateService;
 import com.epam.rft.atsy.service.domain.CandidateDTO;
 import com.epam.rft.atsy.web.controllers.rest.CandidateController;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
@@ -29,6 +33,9 @@ public class CandidateControllerTest {
     @Mock
     CandidateService candidateService;
 
+    @Mock
+    ObjectMapper objectMapper;
+
 
     @BeforeMethod
     public void setUp() {
@@ -36,19 +43,24 @@ public class CandidateControllerTest {
     }
 
     @Test
-    public void loadPageTest() {
-//
-//        //given
-//        FilterRequest sortingRequest = new FilterRequest();
-//        sortingRequest.setFieldName("name");
-//        sortingRequest.setOrder(SortingRequest.Order.ASC);
-//        given(candidateService.getAllCandidate(sortingRequest)).willReturn(Arrays.asList(new CandidateDTO("name", "email", "phome", "description", "referer", new Short("1"))));
-//
-//        //when
-//        Collection<CandidateDTO> result = candidateController.loadPage("", "ASC", "name");
-//
-//        //then
-//        assertThat(result, containsInAnyOrder(new CandidateDTO("name", "email", "phome", "description", "referer", new Short("1"))));
+    public void loadPageTest() throws IOException {
+
+        //given
+        FilterRequest filterRequest = new FilterRequest();
+        filterRequest.setFieldName("name");
+        filterRequest.setOrder(SortingRequest.Order.ASC);
+        Map<String,String> filterMap = new HashMap<>();
+        filterMap.put("name","test");
+        filterRequest.setFilters(filterMap);
+        given(candidateService.getAllCandidate(filterRequest)).willReturn(Arrays.asList(new CandidateDTO("test", "email", "phone", "description", "referer", new Short("1"))));
+        String filterJson="{\"name\":\"test\"}";
+        given(objectMapper.readValue(filterJson,Map.class)).willReturn(filterMap);
+
+        //when
+        Collection<CandidateDTO> result = candidateController.loadPage(filterJson, "ASC", "name");
+
+        //then
+        assertThat(result, containsInAnyOrder(new CandidateDTO("test", "email", "phone", "description", "referer", new Short("1"))));
     }
 
     @Test(expectedExceptions = NullPointerException.class)
