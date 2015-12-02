2322f062666d46d59ccf11b1187b8c6f3122c8bf
matesszabo <matesszabo@hotmail.com>
2015-12-02 15:56:13 +0100

    Additional test method for LoginController

--- web/src/test/java/com/epam/rft/atsy/web/controllers/LoginControllerTest.java
+++ web/src/test/java/com/epam/rft/atsy/web/controllers/LoginControllerTest.java
@@ -33,6 +33,8 @@ public class LoginControllerTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoginControllerTest.class);
     private static final Locale LOCALE = Locale.UK;
+    private static final String REDIRECT_PARAM = "redirect";
+
     @InjectMocks
     LoginController loginController;
 
@@ -145,5 +147,22 @@ public class LoginControllerTest {
 
         //then
         assertThat(model.getViewName(),is(equalToIgnoringCase("login")));
+    }
+
+    @Test
+    public void resolveRedirectStringUtilsIsNotBlank() throws UserNotFoundException{
+        given(userDTO.getName()).willReturn("test");
+        given(userDTO.getPassword()).willReturn("pass1");
+        given(userService.login(userDTO)).willReturn(userDTO);
+        given(bindingResult.hasErrors()).willReturn(false);
+        given(request.getSession()).willReturn(session);
+        given(request.getParameter(REDIRECT_PARAM)).willReturn("anything");
+        doNothing().when(session).setAttribute("user", userDTO);
+        //when
+        ModelAndView model = loginController.handleLogin(userDTO, LOCALE, bindingResult, request);
+
+        //then
+        assertThat(model.getViewName(), containsString("redirect:"));
+
     }
 }
