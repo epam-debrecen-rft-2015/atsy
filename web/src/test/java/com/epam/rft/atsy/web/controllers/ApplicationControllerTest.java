796307bef264aff069594a866a91a92c8511d5fd
matesszabo <matesszabo@hotmail.com>
2015-12-04 00:11:02 +0100

    ApplicationControllerTest created

---
+++ web/src/test/java/com/epam/rft/atsy/web/controllers/ApplicationControllerTest.java
@@ -0,0 +1,30 @@
+package com.epam.rft.atsy.web.controllers;
+
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.web.servlet.ModelAndView;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+/**
+ * Created by mates on 2015. 12. 04..
+ */
+public class ApplicationControllerTest {
+    @InjectMocks
+    private ApplicationController underTest;
+
+    @BeforeMethod
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void shouldReturnViewModel() {
+        //when
+        ModelAndView model = underTest.loadPage();
+        //then
+        assertThat(model.getViewName(), is("application"));
+    }
+}
