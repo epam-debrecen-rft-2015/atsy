13bf5d24755b250eb0b3b7c9ae48bf26699648d0
Daniel Toth <dponttoth@gmail.com>
2015-12-14 15:23:25 +0100

    Set states locale

--- web/src/main/java/com/epam/rft/atsy/web/controllers/rest/CandidateApplicationController.java
+++ web/src/main/java/com/epam/rft/atsy/web/controllers/rest/CandidateApplicationController.java
@@ -2,6 +2,7 @@ package com.epam.rft.atsy.web.controllers.rest;
 
 import com.epam.rft.atsy.service.ApplicationService;
 import com.epam.rft.atsy.service.domain.CandidateApplicationDTO;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.Collection;
+import java.util.Locale;
 
 /**
  * Created by tothd on 2015. 12. 09..
@@ -17,13 +19,23 @@ import java.util.Collection;
 @RequestMapping(value = "/secure/applications/{candidateId}")
 public class CandidateApplicationController {
 
+    private static final String APPLICATION_STATE= "candidate.table.state.";
 
     @Resource
     private ApplicationService applicationService;
 
+    @Resource
+    private MessageSource messageSource;
 
     @RequestMapping(method = RequestMethod.GET)
-    public Collection<CandidateApplicationDTO> loadApplications(@PathVariable(value = "candidateId") Long candidateId) {
-        return applicationService.getStatesByCandidateId(candidateId);
+    public Collection<CandidateApplicationDTO> loadApplications(@PathVariable(value = "candidateId") Long candidateId, Locale locale) {
+        Collection<CandidateApplicationDTO> applicationStates = applicationService.getStatesByCandidateId(candidateId);
+
+        for(CandidateApplicationDTO candidateApplicationDTO : applicationStates){
+            String stateType = candidateApplicationDTO.getStateType();
+            stateType=messageSource.getMessage(APPLICATION_STATE+stateType,new Object[]{stateType},locale);
+            candidateApplicationDTO.setStateType(stateType);
+        }
+        return applicationStates;
     }
 }
