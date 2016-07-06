61bbabe74ea9f7b28e422b3f8f4b2faeefc9e291
Attila Bagossy <bagossyattila@outlook.com>
2016-07-06 14:47:04 +0200

    added second test case to application repository test

--- persistence/src/test/java/com/epam/rft/atsy/persistence/repositories/ApplicationsRepositoryIT.java
+++ persistence/src/test/java/com/epam/rft/atsy/persistence/repositories/ApplicationsRepositoryIT.java
@@ -3,10 +3,13 @@ package com.epam.rft.atsy.persistence.repositories;
 
 import com.epam.rft.atsy.persistence.entities.ApplicationEntity;
 import com.epam.rft.atsy.persistence.entities.CandidateEntity;
+import com.epam.rft.atsy.persistence.entities.ChannelEntity;
+import com.epam.rft.atsy.persistence.entities.PositionEntity;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.util.Date;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
@@ -16,6 +19,7 @@ import static org.junit.Assert.assertThat;
 @Sql("classpath:sql/application/application.sql")
 public class ApplicationsRepositoryIT extends AbstractRepositoryIT {
 
+    public static final long CANDIDATE_A_ID = 1L;
     public static final long CANDIDATE_B_ID = 2L;
 
     @Autowired
@@ -36,5 +40,43 @@ public class ApplicationsRepositoryIT extends AbstractRepositoryIT {
         assertThat(result, notNullValue());
         assertThat(result, empty());
     }
+
+    @Test
+    public void findByCandidateEntityShouldFindSingleApplicationForCandidateWithSingleApplication() {
+        // Given
+        CandidateEntity candidateEntityA = this.candidateRepository.findOne(CANDIDATE_A_ID);
+        ChannelEntity expectedChannelEntity = ChannelEntity.builder()
+                .id(1L)
+                .name("direkt")
+                .build();
+        PositionEntity expectedPositionEntity = PositionEntity.builder()
+                .id(1L)
+                .name("Fejlesztő")
+                .build();
+        Date currentDate = new Date();
+
+        // When
+        List<ApplicationEntity> result = this.repository.findByCandidateEntity(candidateEntityA);
+
+        // Then
+        assertThat(result, notNullValue());
+        assertThat(result.size(), is(1));
+
+        ApplicationEntity application = result.get(0);
+        assertThat(application, notNullValue());
+
+        assertThat(application.getCandidateEntity(), notNullValue());
+        assertThat(application.getCandidateEntity(), is(candidateEntityA));
+
+        assertThat(application.getChannelEntity(), notNullValue());
+        assertThat(application.getChannelEntity(), is(expectedChannelEntity));
+
+        assertThat(application.getPositionEntity(), notNullValue());
+        assertThat(application.getPositionEntity(), is(expectedPositionEntity));
+
+        assertThat(application.getCreationDate(), notNullValue());
+        assertThat(application.getCreationDate(), lessThanOrEqualTo(currentDate));
+    }
+
 
 }
--- persistence/src/test/resources/sql/application/application.sql
+++ persistence/src/test/resources/sql/application/application.sql
@@ -1,2 +1,2 @@
 
-INSERT INTO applications (candidate_id, position_id, channel_id) VALUES (1,1,1);
+INSERT INTO applications (candidate_id, position_id, channel_id, creation_date) VALUES (1,1,1, NOW());
