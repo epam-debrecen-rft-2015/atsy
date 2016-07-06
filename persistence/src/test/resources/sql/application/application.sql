48a7c6c3e3d8bec95c1730e8361093bcffaec389
Attila Bagossy <bagossyattila@outlook.com>
2016-07-06 15:04:51 +0200

    cleanup test sql for application repository integration test

--- persistence/src/test/resources/sql/application/application.sql
+++ persistence/src/test/resources/sql/application/application.sql
@@ -1,6 +1,17 @@
 
-INSERT INTO applications (candidate_id, position_id, channel_id, creation_date) VALUES (1,1,1, NOW());
-INSERT INTO applications (candidate_id, position_id, channel_id, creation_date) VALUES
-    (3,1,2, NOW()),
-    (3,1,3, NOW()),
-    (3,1,4, NOW());
+INSERT INTO applications (candidate_id, position_id, channel_id, creation_date)
+    SELECT c.id, p.id, ch.id, NOW() FROM
+        candidates c, positions p, channels ch
+    WHERE
+        c.name = 'Candidate A'
+        AND p.name = 'Fejlesztő'
+        AND ch.name = 'direkt';
+
+INSERT INTO applications (candidate_id, position_id, channel_id, creation_date)
+    SELECT c.id, p.id, ch.id, NOW() FROM
+        candidates c, positions p, channels ch
+    WHERE
+        c.name = 'Candidate C'
+        AND p.name = 'Fejlesztő'
+        AND ch.name in ('profession hírdetés', 'profession adatbázis', 'facebook')
+    ORDER BY c.id, p.id, ch.id;
