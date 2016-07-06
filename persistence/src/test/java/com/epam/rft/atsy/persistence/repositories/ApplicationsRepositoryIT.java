ac8886f05379a87429dbdfa9608f8cd78d0bc420
Attila Bagossy <bagossyattila@outlook.com>
2016-07-06 14:28:13 +0200

    clean up application repository integration test case

--- persistence/src/test/java/com/epam/rft/atsy/persistence/repositories/ApplicationsRepositoryIT.java
+++ persistence/src/test/java/com/epam/rft/atsy/persistence/repositories/ApplicationsRepositoryIT.java
@@ -3,15 +3,20 @@ package com.epam.rft.atsy.persistence.repositories;
 
 import com.epam.rft.atsy.persistence.entities.ApplicationEntity;
 import com.epam.rft.atsy.persistence.entities.CandidateEntity;
-import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
 @Sql("classpath:sql/application/application.sql")
 public class ApplicationsRepositoryIT extends AbstractRepositoryIT {
+
+    public static final long CANDIDATE_B_ID = 2L;
 
     @Autowired
     private ApplicationsRepository repository;
@@ -20,13 +25,16 @@ public class ApplicationsRepositoryIT extends AbstractRepositoryIT {
     private CandidateRepository candidateRepository;
 
     @Test
-    public void findByCandidateEntityShouldNotFindCandidatesByUnknownCandidate() {
+    public void findByCandidateEntityShouldNotFindApplicationForCandidateWithoutApplications() {
+        // Given
+        CandidateEntity candidateB = this.candidateRepository.findOne(CANDIDATE_B_ID);
 
-        CandidateEntity candidate = this.candidateRepository.findOne(2L);
-        List<ApplicationEntity> result = this.repository.findByCandidateEntity(candidate);
+        // When
+        List<ApplicationEntity> result = this.repository.findByCandidateEntity(candidateB);
 
-        Assert.assertNotNull(result);
-        Assert.assertTrue(result.isEmpty());
+        // Then
+        assertThat(result, notNullValue());
+        assertThat(result, empty());
     }
 
 }
