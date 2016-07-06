bc2277d3a19161189697d32ebaecb1a9aff2863f
Iványi-Nagy Gábor <gabilampard@gmail.com>
2016-07-06 11:09:17 +0200

    Created Builder interface with build() method Created generic AbstractStateBuilder class
    Removed long parameters list constructor with @Builder annotation from StateDTO and StateViewDTO classes
    These two classes extends AbstractStateBuilder class

---
+++ service/src/main/java/com/epam/rft/atsy/service/domain/states/builder/AbstractStateBuilder.java
@@ -0,0 +1,88 @@
+package com.epam.rft.atsy.service.domain.states.builder;
+
+import com.epam.rft.atsy.service.domain.ApplicationDTO;
+import com.epam.rft.atsy.service.domain.PositionDTO;
+import com.epam.rft.atsy.service.domain.states.AbstractStateDTO;
+import lombok.AllArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * Created by Gabor_Ivanyi-Nagy on 7/6/2016.
+ */
+
+
+@AllArgsConstructor
+public abstract class AbstractStateBuilder<B extends AbstractStateBuilder<B, T>, T extends AbstractStateDTO> implements Builder<T> {
+
+    protected String name;
+    protected T object;
+
+
+    public B id(Long id) {
+        object.setId(id);
+        return (B) this;
+    }
+
+    public B candidateId(Long candidateId) {
+        object.setCandidateId(candidateId);
+        return (B) this;
+    }
+
+    public B position(PositionDTO position) {
+        object.setPosition(position);
+        return (B) this;
+    }
+
+    public B applicationDTO(ApplicationDTO applicationDTO) {
+        object.setApplicationDTO(applicationDTO);
+        return (B) this;
+    }
+
+    public B languageSkill(Short languageSkill) {
+        object.setLanguageSkill(languageSkill);
+        return (B) this;
+    }
+
+    public B description(String description) {
+        object.setDescription(description);
+        return (B) this;
+    }
+
+    public B result(String result) {
+        object.setResult(result);
+        return (B) this;
+    }
+
+    public B offeredMoney(Long offeredMoney) {
+        object.setOfferedMoney(offeredMoney);
+        return (B) this;
+    }
+
+    public B claim(Long claim) {
+        object.setClaim(claim);
+        return (B) this;
+    }
+
+    public B feedbackDate(Date feedbackDate) {
+        object.setFeedbackDate(feedbackDate);
+        return (B) this;
+    }
+
+    public B stateType(String stateType) {
+        object.setStateType(stateType);
+        return (B) this;
+    }
+
+    public B stateIndex(Integer stateIndex) {
+        object.setStateIndex(stateIndex);
+        return (B) this;
+    }
+
+
+    @Override
+    public T build() {
+        return object;
+    }
+
+}
