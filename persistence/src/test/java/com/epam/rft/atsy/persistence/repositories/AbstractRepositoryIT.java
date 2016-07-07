d960b8c845c905004bcbb38dd4eaeec3f932316e
Bertalan Kis <kbertalan@gmail.com>
2016-07-07 11:09:46 +0200

    fix windows character encoding issue by forcing utf-8 encoding for test sql scripts

--- persistence/src/test/java/com/epam/rft/atsy/persistence/repositories/AbstractRepositoryIT.java
+++ persistence/src/test/java/com/epam/rft/atsy/persistence/repositories/AbstractRepositoryIT.java
@@ -4,6 +4,7 @@ import com.epam.rft.atsy.persistence.configuration.PersistenceConfiguration;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,5 +12,6 @@ import org.springframework.transaction.annotation.Transactional;
 @ActiveProfiles("integration")
 @ContextConfiguration(classes = PersistenceConfiguration.class, initializers = RepositoryITApplciationContextInitializer.class)
 @Transactional
+@SqlConfig(encoding = "UTF-8")
 public abstract class AbstractRepositoryIT {
 }
