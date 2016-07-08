852c4189588cf8e67166b8ea143aa02654c314cf
Besenyei Szabolcs <besenyei.szabolcs@nyfbox.hu>
2016-07-08 15:48:10 +0200

    Finished covering all possible test cases

--- persistence/src/test/java/com/epam/rft/atsy/persistence/repositories/CandidateRepositoryIT.java
+++ persistence/src/test/java/com/epam/rft/atsy/persistence/repositories/CandidateRepositoryIT.java
@@ -1,6 +1,7 @@
 package com.epam.rft.atsy.persistence.repositories;
 
 import com.epam.rft.atsy.persistence.entities.CandidateEntity;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
@@ -8,8 +9,11 @@ import org.springframework.data.domain.Sort;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.empty;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
@@ -22,6 +26,9 @@ public class CandidateRepositoryIT extends AbstractRepositoryIT {
     private static final String INVALID_NAME_SEARCH = "date X";
     private static final String INVALID_EMAIL_SEARCH = "@dummydomain.com";
     private static final String INVALID_PHONE_NUMBER_SEARCH = "+3699";
+
+    private static final String NON_EXISTENT_CANDIDATE_NAME = "Candidate Foo";
+    private static final String EXISTING_CANDIDATE_NAME = "Candidate A";
 
     private static final List<CandidateEntity> POSITIVE_CANDIDATES_TEST =
             Arrays.asList(
@@ -76,7 +83,7 @@ public class CandidateRepositoryIT extends AbstractRepositoryIT {
     }
 
     @Test
-    public void findAllCandidateyByFilterRequestWithValidDataSortedByNameInDescendingOrder() {
+    public void findAllCandidateByFilterRequestWithValidDataSortedByNameInDescendingOrder() {
         for (CandidateEntity candidate : POSITIVE_CANDIDATES_TEST) {
             //Given
             final String name = candidate.getName();
@@ -93,6 +100,113 @@ public class CandidateRepositoryIT extends AbstractRepositoryIT {
             //Then
             assertCandidateEntityListWithSorting(result, name, email, phone, createSortOrder(Sort.Direction.DESC, "name"));
         }
+    }
+
+    @Test
+    public void findAllCandidatesByFilterRequestWithValidDataSortedByEmailInAscendingOrder() {
+        for (CandidateEntity candidate : POSITIVE_CANDIDATES_TEST) {
+            //Given
+            final String name = candidate.getName();
+            final String email = candidate.getEmail();
+            final String phone = candidate.getPhone();
+
+            //When
+            List<CandidateEntity> result = this.candidateRepository.findAllCandidatesByFilterRequest(
+                    name,
+                    email,
+                    phone,
+                    new Sort(createSortOrder(Sort.Direction.ASC, "email")));
+
+            //Then
+            assertCandidateEntityListWithSorting(result, name, email, phone, createSortOrder(Sort.Direction.ASC, "email"));
+        }
+    }
+
+    @Test
+    public void findAllCandidatesByFilterRequestWithValidDataSortedByEmailInDescendingOrder() {
+        for (CandidateEntity candidate : POSITIVE_CANDIDATES_TEST) {
+            //Given
+            final String name = candidate.getName();
+            final String email = candidate.getEmail();
+            final String phone = candidate.getPhone();
+
+            //When
+            List<CandidateEntity> result = this.candidateRepository.findAllCandidatesByFilterRequest(
+                    name,
+                    email,
+                    phone,
+                    new Sort(createSortOrder(Sort.Direction.DESC, "email")));
+
+            //Then
+            assertCandidateEntityListWithSorting(result, name, email, phone, createSortOrder(Sort.Direction.DESC, "email"));
+        }
+    }
+
+    @Test
+    public void findAllCandidatesByFilterRequestWithValidDataSortedByPhoneInAscendingOrder() {
+        for (CandidateEntity candidate : POSITIVE_CANDIDATES_TEST) {
+            //Given
+            final String name = candidate.getName();
+            final String email = candidate.getEmail();
+            final String phone = candidate.getPhone();
+
+            //When
+            List<CandidateEntity> result = this.candidateRepository.findAllCandidatesByFilterRequest(
+                    name,
+                    email,
+                    phone,
+                    new Sort(createSortOrder(Sort.Direction.ASC, "phone")));
+
+            //Then
+            assertCandidateEntityListWithSorting(result, name, email, phone, createSortOrder(Sort.Direction.ASC, "phone"));
+        }
+    }
+
+    @Test
+    public void findAllCandidatesByFilterRequestWithValidDataSortedByPhoneInDescendingOrder() {
+        for (CandidateEntity candidate : POSITIVE_CANDIDATES_TEST) {
+            //Given
+            final String name = candidate.getName();
+            final String email = candidate.getEmail();
+            final String phone = candidate.getPhone();
+
+            //When
+            List<CandidateEntity> result = this.candidateRepository.findAllCandidatesByFilterRequest(
+                    name,
+                    email,
+                    phone,
+                    new Sort(createSortOrder(Sort.Direction.DESC, "phone")));
+
+            //Then
+            assertCandidateEntityListWithSorting(result, name, email, phone, createSortOrder(Sort.Direction.DESC, "phone"));
+        }
+    }
+
+    @Test
+    public void findAllCandidateByFilterRequestWithInvalidDataSortedByNameInAscendingOrder() {
+        for (CandidateEntity candidate : NEGATIVE_CANDIDATES_TEST) {
+            //Given
+            final String name = candidate.getName();
+            final String email = candidate.getEmail();
+            final String phone = candidate.getPhone();
+
+            //When
+            List<CandidateEntity> result = this.candidateRepository.findAllCandidatesByFilterRequest(
+                    name,
+                    email,
+                    phone,
+                    //sorting is redundant on an empty set, but needed to call the method
+                    new Sort(createSortOrder(Sort.Direction.DESC, "name")));
+
+            //Then
+            assertCandidateEntityListWithInvalidData(result);
+
+        }
+    }
+
+    private void assertCandidateEntityListWithInvalidData(List<CandidateEntity> candidates) {
+        assertThat(candidates, notNullValue());
+        assertThat(candidates, empty());
     }
 
     private void assertCandidateEntityListWithSorting(List<CandidateEntity> candidates,
@@ -190,5 +304,24 @@ public class CandidateRepositoryIT extends AbstractRepositoryIT {
         }
 
         assertTrue(invariant);
+    }
+
+    @Test
+    public void findByNameShouldNotFindNonexistentCandidate() {
+        //When
+        CandidateEntity candidate = this.candidateRepository.findByName(NON_EXISTENT_CANDIDATE_NAME);
+
+        //Then
+        assertThat(candidate, nullValue());
+    }
+
+    @Test
+    public void findByNameShouldFindExistingCandidate() {
+        //When
+        CandidateEntity candidate = this.candidateRepository.findByName(EXISTING_CANDIDATE_NAME);
+
+        //Then
+        assertThat(candidate, notNullValue());
+        assertThat(candidate.getName(), is(EXISTING_CANDIDATE_NAME));
     }
 }
