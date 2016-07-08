6c36e2c618ae2214fca9c973a0ce92f8a7077d4c
István Lakatos <atyep95@msn.com>
2016-07-08 13:13:21 +0200

    Removed unused import

--- persistence/src/test/java/com/epam/rft/atsy/persistence/repositories/PasswordHistoryRepositoryIT.java
+++ persistence/src/test/java/com/epam/rft/atsy/persistence/repositories/PasswordHistoryRepositoryIT.java
@@ -2,7 +2,6 @@ package com.epam.rft.atsy.persistence.repositories;
 
 import com.epam.rft.atsy.persistence.entities.PasswordHistoryEntity;
 import com.epam.rft.atsy.persistence.entities.UserEntity;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
