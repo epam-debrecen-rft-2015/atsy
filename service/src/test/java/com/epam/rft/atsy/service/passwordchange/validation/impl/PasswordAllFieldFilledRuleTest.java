030fba4ade9f1de98ee34e8a27655a421ff7f439
Besenyei Szabolcs <besenyei.szabolcs@nyfbox.hu>
2016-07-11 14:22:50 +0200

    Fix erroneous return caused by copy-pasting

---
+++ service/src/test/java/com/epam/rft/atsy/service/passwordchange/validation/impl/PasswordAllFieldFilledRuleTest.java
@@ -0,0 +1,9 @@
+package com.epam.rft.atsy.service.passwordchange.validation.impl;
+
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PasswordAllFieldFilledRuleTest {
+
+}
