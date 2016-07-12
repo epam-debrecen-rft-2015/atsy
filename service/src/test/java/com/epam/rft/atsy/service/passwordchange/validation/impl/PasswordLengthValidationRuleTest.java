fac2b1e9b472fdd271031e6e3da0e9390d5da408
István Lakatos <atyep95@msn.com>
2016-07-12 09:18:16 +0200

    Fixed problems written in pull request comments

--- service/src/test/java/com/epam/rft/atsy/service/passwordchange/validation/impl/PasswordLengthValidationRuleTest.java
+++ service/src/test/java/com/epam/rft/atsy/service/passwordchange/validation/impl/PasswordLengthValidationRuleTest.java
@@ -1,14 +1,12 @@
 package com.epam.rft.atsy.service.passwordchange.validation.impl;
 
 import com.epam.rft.atsy.service.domain.PasswordChangeDTO;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -18,10 +16,10 @@ public class PasswordLengthValidationRuleTest {
     private static final String PASSWORD_WITH_LENGTH_EQUAL_TO_MIN = "foobar";
     private static final String PASSWORD_WITH_LENGTH_OVER_MIN = "foobarbuz";
 
-    private static PasswordLengthValidationRule rule;
+    private PasswordLengthValidationRule rule;
 
-    @BeforeClass
-    public static void setUp() {
+    @Before
+    public void setUp() {
         rule = new PasswordLengthValidationRule();
     }
 
@@ -36,7 +34,6 @@ public class PasswordLengthValidationRuleTest {
         boolean result = rule.isValid(passwordChangeDtoUnderMinLength);
 
         //Then
-        assertThat(result, notNullValue());
         assertFalse(result);
     }
 
@@ -50,7 +47,6 @@ public class PasswordLengthValidationRuleTest {
         boolean result = rule.isValid(passwordChangeDtoExactMinLength);
 
         //Then
-        assertThat(result, notNullValue());
         assertTrue(result);
     }
 
@@ -65,7 +61,6 @@ public class PasswordLengthValidationRuleTest {
         boolean result = rule.isValid(passwordChangeDtoOverMinLength);
 
         //Then
-        assertThat(result, notNullValue());
         assertTrue(result);
     }
 
