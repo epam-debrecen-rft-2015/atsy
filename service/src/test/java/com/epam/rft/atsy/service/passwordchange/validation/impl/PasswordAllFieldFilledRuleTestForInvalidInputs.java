a65440824eb20bd78c068147fa6438bec44301ae
Besenyei Szabolcs <besenyei.szabolcs@nyfbox.hu>
2016-07-12 14:30:46 +0200

    Decompose positive and negative test cases into different classes

---
+++ service/src/test/java/com/epam/rft/atsy/service/passwordchange/validation/impl/PasswordAllFieldFilledRuleTestForInvalidInputs.java
@@ -0,0 +1,89 @@
+package com.epam.rft.atsy.service.passwordchange.validation.impl;
+
+import com.epam.rft.atsy.service.domain.PasswordChangeDTO;
+import com.google.common.collect.*;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(Parameterized.class)
+public class PasswordAllFieldFilledRuleTestForInvalidInputs {
+
+    private PasswordAllFieldFilledRule rule;
+
+    private static final String EMPTY_STRING = StringUtils.EMPTY;
+    private static final String BLANK_SPACES = "    ";
+    private static final String LINE_FEED = "\u005Cn";
+    private static final String CARRIAGE_RETURN = "\u005Cr";
+    private static final String CARRIAGE_RETURN_WITH_LINE_FEED = CARRIAGE_RETURN + LINE_FEED;
+    private static final String HORIZONTAL_TABULATOR = "\u005Ct";
+
+    private static final String CONTROL_POSITIVE = "foo";
+
+    @Parameter
+    public String newPassword;
+
+
+    @Parameter(value = 1)
+    public String newPasswordConfirm;
+
+    @Parameter(value = 2)
+    public String oldPassword;
+
+    @Parameter(value = 3)
+    public boolean expected;
+
+    @Before
+    public void init() {
+        rule = new PasswordAllFieldFilledRule();
+    }
+
+    @Test
+    public void isValidShouldReturnFalseForAllNegativeInputs() {
+        //Given
+        PasswordChangeDTO passwordChangeDTO = PasswordChangeDTO.builder()
+                .newPassword(this.newPassword)
+                .newPasswordConfirm(this.newPasswordConfirm)
+                .oldPassword(this.oldPassword)
+                .build();
+
+        //Then
+        assertThat(rule.isValid(passwordChangeDTO), is(false));
+    }
+
+    @Parameters(name = "{index}: ({0},{1},{2}) -> {3}")
+    public static Collection<Object[]> invalidTestData() {
+
+        final Set<Object> whiteSpaces =
+                ImmutableSet.of(EMPTY_STRING, BLANK_SPACES, LINE_FEED, CARRIAGE_RETURN, CARRIAGE_RETURN_WITH_LINE_FEED, HORIZONTAL_TABULATOR);
+
+        List<Object[]> domain = Sets.powerSet(whiteSpaces)
+                .stream()
+                .filter(s -> s.size() <= 3 && !s.isEmpty())
+                .map(ArrayList::new)
+                .map(s -> {
+                    while (s.size() < 3) {
+                        s.add(CONTROL_POSITIVE);
+                    }
+                    s.add(false); //add the expected test result
+                    return s.toArray();
+                })
+                .collect(Collectors.toList());
+
+         return domain;
+    }
+}
