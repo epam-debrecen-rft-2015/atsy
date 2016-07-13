4c0fb984e02c998c8a7835cbeb62bd811cb89624
István Lakatos <atyep95@msn.com>
2016-07-13 13:01:08 +0200

    Root path now redirects to login page or to secure welcome page

---
+++ web/src/main/java/com/epam/rft/atsy/web/controllers/RootRedirectionController.java
@@ -0,0 +1,26 @@
+package com.epam.rft.atsy.web.controllers;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class RootRedirectionController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView pageLoad() {
+
+        if (SecurityContextHolder.getContext().getAuthentication() != null &&
+                SecurityContextHolder.getContext().getAuthentication().isAuthenticated()) {
+
+            return new ModelAndView("redirect:/secure/welcome");
+
+        } else {
+            return new ModelAndView("redirect:/login");
+        }
+
+    }
+}
