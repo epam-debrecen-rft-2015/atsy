b70fea2769462cce992cc0bf3fa4fdb7ec7772ab
Besenyei Szabolcs <besza.elte@gmail.com>
2016-07-13 16:13:31 -0400

    Rework testing logic

--- persistence/src/test/java/com/epam/rft/atsy/persistence/repositories/CandidateRepositoryIT.java
+++ persistence/src/test/java/com/epam/rft/atsy/persistence/repositories/CandidateRepositoryIT.java
@@ -1,11 +1,13 @@
 package com.epam.rft.atsy.persistence.repositories;
 
 import com.epam.rft.atsy.persistence.entities.CandidateEntity;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.*;
@@ -16,203 +18,78 @@ import static org.junit.Assert.assertTrue;
 
 public class CandidateRepositoryIT extends AbstractRepositoryIT {
 
-    private static final String VALID_NAME_SEARCH = "date A";
-    private static final String VALID_EMAIL_SEARCH = "atsy.";
-    private static final String VALID_PHONE_NUMBER_SEARCH = "77";
+    private static final Long CANDIDATE_A_ID = 1L;
+    private static final Long CANDIDATE_B_ID = 2L;
+    private static final Long CANDIDATE_C_ID = 3L;
 
-    private static final String INVALID_NAME_SEARCH = "date X";
-    private static final String INVALID_EMAIL_SEARCH = "@dummydomain.com";
-    private static final String INVALID_PHONE_NUMBER_SEARCH = "+3699";
+    private static final String CANDIDATE_A_NAME = "Candidate A";
+    private static final String CANDIDATE_B_NAME = "Candidate B";
+    private static final String CANDIDATE_C_NAME = "Candidate C";
 
-    private static final String NON_EXISTENT_CANDIDATE_NAME = "Candidate Foo";
-    private static final String EXISTING_CANDIDATE_NAME = "Candidate A";
+    private static final String CANDIDATE_A_EMAIL = "candidate.a@atsy.com";
+    private static final String CANDIDATE_B_EMAIL = "candidate.b@atsy.com";
+    private static final String CANDIDATE_C_EMAIL = "candidate.c@atsy.com";
 
-    private static final List<CandidateEntity> POSITIVE_CANDIDATES_TEST =
-            Arrays.asList(
-                    CandidateEntity.builder().name("").email("").phone("").build(),
-                    CandidateEntity.builder().name("").email("").phone(VALID_PHONE_NUMBER_SEARCH).build(),
-                    CandidateEntity.builder().name("").email(VALID_EMAIL_SEARCH).phone("").build(),
-                    CandidateEntity.builder().name(VALID_NAME_SEARCH).email("").phone("").build(),
-                    CandidateEntity.builder().name("").email(VALID_EMAIL_SEARCH).phone(VALID_PHONE_NUMBER_SEARCH).build(),
-                    CandidateEntity.builder().name(VALID_NAME_SEARCH).email("").phone(VALID_PHONE_NUMBER_SEARCH).build(),
-                    CandidateEntity.builder().name(VALID_NAME_SEARCH).email(VALID_EMAIL_SEARCH).phone("").build(),
-                    CandidateEntity.builder().name(VALID_NAME_SEARCH).email(VALID_EMAIL_SEARCH).phone(VALID_PHONE_NUMBER_SEARCH).build()
+    private static final String CANDIDATE_A_PHONE = "+36105555555";
+    private static final String CANDIDATE_B_PHONE = "+36106666666";
+    private static final String CANDIDATE_C_PHONE = "+36107777777";
 
-            );
+    private static final String CANDIDATE_A_DESCRIPTION = "Elegáns, kicsit furi";
+    private static final String CANDIDATE_B_DESCRIPTION = "Össze-vissza beszélt";
+    private static final String CANDIDATE_C_DESCRIPTION = StringUtils.EMPTY;
 
-    private static final List<CandidateEntity> NEGATIVE_CANDIDATES_TEST =
-            Arrays.asList(
-                    CandidateEntity.builder().name("").email("").phone(INVALID_PHONE_NUMBER_SEARCH).build(),
-                    CandidateEntity.builder().name("").email(INVALID_EMAIL_SEARCH).phone("").build(),
-                    CandidateEntity.builder().name(INVALID_NAME_SEARCH).email("").phone("").build(),
-                    CandidateEntity.builder().name("").email(INVALID_EMAIL_SEARCH).phone(INVALID_PHONE_NUMBER_SEARCH).build(),
-                    CandidateEntity.builder().name(INVALID_NAME_SEARCH).email("").phone(INVALID_PHONE_NUMBER_SEARCH).build(),
-                    CandidateEntity.builder().name(INVALID_NAME_SEARCH).email(INVALID_EMAIL_SEARCH).phone("").build(),
-                    CandidateEntity.builder().name(INVALID_NAME_SEARCH).email(INVALID_EMAIL_SEARCH).phone(INVALID_PHONE_NUMBER_SEARCH).build()
+    private static final String CANDIDATE_A_REFERER = "google";
+    private static final String CANDIDATE_B_REFERER = StringUtils.EMPTY;
+    private static final String CANDIDATE_C_REFERER = "haverok";
 
-            );
+    private static final Short CANDIDATE_A_LANGUAGE_SKILL = 5;
+    private static final Short CANDIDATE_B_LANGUAGE_SKILL = 2;
+    private static final Short CANDIDATE_C_LANGUAGE_SKILL = 8;
 
-    private Sort.Order createSortOrder(Sort.Direction direction, String property) {
-        return new Sort.Order(direction, property);
-    }
+    private static final String EXISTING_CANDIDATE_NAME = CANDIDATE_A_NAME;
+
+    private static final String NON_EXISTENT_CANDIDATE_NAME = "Candidate X";
+
+    private static final CandidateEntity candidateA = CandidateEntity.builder()
+            .id(CANDIDATE_A_ID)
+            .name(CANDIDATE_A_NAME)
+            .email(CANDIDATE_A_EMAIL)
+            .phone(CANDIDATE_A_PHONE)
+            .description(CANDIDATE_A_DESCRIPTION)
+            .referer(CANDIDATE_A_REFERER)
+            .languageSkill(CANDIDATE_A_LANGUAGE_SKILL).build();
+
+
+    private static final CandidateEntity candidateB = CandidateEntity.builder()
+            .id(CANDIDATE_B_ID)
+            .name(CANDIDATE_B_NAME)
+            .email(CANDIDATE_B_EMAIL)
+            .phone(CANDIDATE_B_PHONE)
+            .description(CANDIDATE_B_DESCRIPTION)
+            .referer(CANDIDATE_B_REFERER)
+            .languageSkill(CANDIDATE_B_LANGUAGE_SKILL)
+            .build();
+
+    private static final CandidateEntity candidateC = CandidateEntity.builder()
+            .id(CANDIDATE_C_ID)
+            .name(CANDIDATE_C_NAME)
+            .email(CANDIDATE_C_EMAIL)
+            .phone(CANDIDATE_C_PHONE)
+            .description(CANDIDATE_C_DESCRIPTION)
+            .referer(CANDIDATE_C_REFERER)
+            .languageSkill(CANDIDATE_C_LANGUAGE_SKILL)
+            .build();
+
+
 
     @Autowired
     private CandidateRepository candidateRepository;
 
-    @Test
-    public void findAllCandidatesByFilterRequestWithValidDataSortedByNameInAscendingOrder() {
-        for (CandidateEntity candidate : POSITIVE_CANDIDATES_TEST) {
-            //Given
-            final String name = candidate.getName();
-            final String email = candidate.getEmail();
-            final String phone = candidate.getPhone();
-
-            //When
-            List<CandidateEntity> result = this.candidateRepository.findAllCandidatesByFilterRequest(
-                    name,
-                    email,
-                    phone,
-                    new Sort(createSortOrder(Sort.Direction.ASC, "name")));
-
-            //Then
-            assertCandidateEntityListWithSorting(result, name, email, phone, createSortOrder(Sort.Direction.ASC, "name"));
-        }
+    private Sort createSort(Sort.Direction direction, String property) {
+        return new Sort(direction, property);
     }
 
-    @Test
-    public void findAllCandidateByFilterRequestWithValidDataSortedByNameInDescendingOrder() {
-        for (CandidateEntity candidate : POSITIVE_CANDIDATES_TEST) {
-            //Given
-            final String name = candidate.getName();
-            final String email = candidate.getEmail();
-            final String phone = candidate.getPhone();
-
-            //When
-            List<CandidateEntity> result = this.candidateRepository.findAllCandidatesByFilterRequest(
-                    name,
-                    email,
-                    phone,
-                    new Sort(createSortOrder(Sort.Direction.DESC, "name")));
-
-            //Then
-            assertCandidateEntityListWithSorting(result, name, email, phone, createSortOrder(Sort.Direction.DESC, "name"));
-        }
-    }
-
-    @Test
-    public void findAllCandidatesByFilterRequestWithValidDataSortedByEmailInAscendingOrder() {
-        for (CandidateEntity candidate : POSITIVE_CANDIDATES_TEST) {
-            //Given
-            final String name = candidate.getName();
-            final String email = candidate.getEmail();
-            final String phone = candidate.getPhone();
-
-            //When
-            List<CandidateEntity> result = this.candidateRepository.findAllCandidatesByFilterRequest(
-                    name,
-                    email,
-                    phone,
-                    new Sort(createSortOrder(Sort.Direction.ASC, "email")));
-
-            //Then
-            assertCandidateEntityListWithSorting(result, name, email, phone, createSortOrder(Sort.Direction.ASC, "email"));
-        }
-    }
-
-    @Test
-    public void findAllCandidatesByFilterRequestWithValidDataSortedByEmailInDescendingOrder() {
-        for (CandidateEntity candidate : POSITIVE_CANDIDATES_TEST) {
-            //Given
-            final String name = candidate.getName();
-            final String email = candidate.getEmail();
-            final String phone = candidate.getPhone();
-
-            //When
-            List<CandidateEntity> result = this.candidateRepository.findAllCandidatesByFilterRequest(
-                    name,
-                    email,
-                    phone,
-                    new Sort(createSortOrder(Sort.Direction.DESC, "email")));
-
-            //Then
-            assertCandidateEntityListWithSorting(result, name, email, phone, createSortOrder(Sort.Direction.DESC, "email"));
-        }
-    }
-
-    @Test
-    public void findAllCandidatesByFilterRequestWithValidDataSortedByPhoneInAscendingOrder() {
-        for (CandidateEntity candidate : POSITIVE_CANDIDATES_TEST) {
-            //Given
-            final String name = candidate.getName();
-            final String email = candidate.getEmail();
-            final String phone = candidate.getPhone();
-
-            //When
-            List<CandidateEntity> result = this.candidateRepository.findAllCandidatesByFilterRequest(
-                    name,
-                    email,
-                    phone,
-                    new Sort(createSortOrder(Sort.Direction.ASC, "phone")));
-
-            //Then
-            assertCandidateEntityListWithSorting(result, name, email, phone, createSortOrder(Sort.Direction.ASC, "phone"));
-        }
-    }
-
-    @Test
-    public void findAllCandidatesByFilterRequestWithValidDataSortedByPhoneInDescendingOrder() {
-        for (CandidateEntity candidate : POSITIVE_CANDIDATES_TEST) {
-            //Given
-            final String name = candidate.getName();
-            final String email = candidate.getEmail();
-            final String phone = candidate.getPhone();
-
-            //When
-            List<CandidateEntity> result = this.candidateRepository.findAllCandidatesByFilterRequest(
-                    name,
-                    email,
-                    phone,
-                    new Sort(createSortOrder(Sort.Direction.DESC, "phone")));
-
-            //Then
-            assertCandidateEntityListWithSorting(result, name, email, phone, createSortOrder(Sort.Direction.DESC, "phone"));
-        }
-    }
-
-    @Test
-    public void findAllCandidateByFilterRequestWithInvalidData() {
-        for (CandidateEntity candidate : NEGATIVE_CANDIDATES_TEST) {
-            //Given
-            final String name = candidate.getName();
-            final String email = candidate.getEmail();
-            final String phone = candidate.getPhone();
-
-            //When
-            List<CandidateEntity> result = this.candidateRepository.findAllCandidatesByFilterRequest(
-                    name,
-                    email,
-                    phone,
-                    null);
-
-            //Then
-            assertCandidateEntityListWithInvalidData(result);
-
-        }
-    }
-
-    private void assertCandidateEntityListWithInvalidData(List<CandidateEntity> candidates) {
-        assertThat(candidates, notNullValue());
-        assertThat(candidates, empty());
-    }
-
-    private void assertCandidateEntityListWithSorting(List<CandidateEntity> candidates,
-                                                      String expectedName,
-                                                      String expectedEmail,
-                                                      String expectedPhone,
-                                                      Sort.Order sorting) {
-
-        assertThat(candidates, notNullValue());
-
+    private void assertCandidateValue(List<CandidateEntity> candidates, String expectedName, String expectedEmail, String expectedPhone) {
         for (CandidateEntity entity : candidates) {
 
             assertThat(entity, notNullValue());
@@ -232,74 +109,45 @@ public class CandidateRepositoryIT extends AbstractRepositoryIT {
                 assertThat(entity.getPhone(), containsString(expectedPhone));
             }
         }
+    }
 
-        boolean invariant = true;
-        if (!candidates.isEmpty()) {
-            CandidateEntity prev = candidates.get(0);
-            if (sorting.getProperty().equals("name")) {
-                if (sorting.isAscending()) {
-                    for (CandidateEntity ent : candidates) {
-                        if (prev.getName().compareTo(ent.getName()) > 0) {
-                            invariant = false;
-                            break;
-                        } else {
-                            prev = ent;
-                        }
-                    }
-                } else {
-                    for (CandidateEntity ent : candidates) {
-                        if (prev.getName().compareTo(ent.getName()) < 0) {
-                            invariant = false;
-                            break;
-                        } else {
-                            prev = ent;
-                        }
-                    }
-                }
-            } else if (sorting.getProperty().equals("email")) {
-                if (sorting.isAscending()) {
-                    for (CandidateEntity ent : candidates) {
-                        if (prev.getEmail().compareTo(ent.getEmail()) > 0) {
-                            invariant = false;
-                            break;
-                        } else {
-                            prev = ent;
-                        }
-                    }
-                } else {
-                    for (CandidateEntity ent : candidates) {
-                        if (prev.getEmail().compareTo(ent.getEmail()) < 0) {
-                            invariant = false;
-                            break;
-                        } else {
-                            prev = ent;
-                        }
-                    }
-                }
-            } else if (sorting.getProperty().equals("phone")) {
-                if (sorting.isAscending()) {
-                    for (CandidateEntity ent : candidates) {
-                        if (prev.getPhone().compareTo(ent.getPhone()) > 0) {
-                            invariant = false;
-                            break;
-                        } else {
-                            prev = ent;
-                        }
-                    }
-                } else {
-                    for (CandidateEntity ent : candidates) {
-                        if (prev.getEmail().compareTo(ent.getEmail()) < 0) {
-                            invariant = false;
-                            break;
-                        } else {
-                            prev = ent;
-                        }
-                    }
-                }
-            }
+    private void assertCandidateList(List<CandidateEntity> expectedCandidates, List<CandidateEntity> actualCandidates) {
+        assertThat(actualCandidates.size(), is(expectedCandidates.size()));
+        for (Iterator expected = expectedCandidates.iterator(), actual = actualCandidates.iterator(); expected.hasNext() && actual.hasNext();) {
+            assertThat(actual.next(), is(expected.next()));
         }
+    }
 
-        assertTrue(invariant);
+    @Test
+    public void findAllCandidatesByFilterRequestShouldFindCandidateAOnlyByPhoneFilter() {
+        //Given
+        String name = "";
+        String email = "";
+        String phone = CANDIDATE_A_PHONE;
+        List<CandidateEntity> expectedCandidates = Arrays.asList(candidateA);
+
+        //When
+        List<CandidateEntity> actualCandidateEntities = candidateRepository.findAllCandidatesByFilterRequest(name, email, phone, null);
+
+        //Then
+        assertCandidateValue(actualCandidateEntities, name, email, phone);
+        assertCandidateList(expectedCandidates, actualCandidateEntities);
+    }
+
+    @Test
+    public void findAllCandidatesByFilterRequestShouldFindThreeCandidatesInDescendingOrderByEmail() {
+        //Given
+        String name = "";
+        String email = "atsy.com";
+        String phone = "";
+        List<CandidateEntity> expectedCandidates = Arrays.asList(candidateC, candidateB, candidateA);
+
+        //When
+        List<CandidateEntity> actualCandidates = candidateRepository.findAllCandidatesByFilterRequest(name, email, phone, createSort(Sort.Direction.DESC, "email"));
+
+        //Then
+        assertCandidateValue(actualCandidates, name, email, phone);
+        assertCandidateList(expectedCandidates, actualCandidates);
     }
 
     @Test
