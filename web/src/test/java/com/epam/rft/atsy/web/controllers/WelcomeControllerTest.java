eedffe9a692d29dcdaedb8283090555f3a71504b
Attila Bagossy <bagossyattila@outlook.com>
2016-07-26 10:40:15 +0200

    Created a new WelcomeControllerTest.

--- web/src/test/java/com/epam/rft/atsy/web/controllers/WelcomeControllerTest.java
+++ web/src/test/java/com/epam/rft/atsy/web/controllers/WelcomeControllerTest.java
@@ -1,28 +1,52 @@
 package com.epam.rft.atsy.web.controllers;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
-import org.springframework.web.servlet.ModelAndView;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+@RunWith(MockitoJUnitRunner.class)
 public class WelcomeControllerTest {
-  @InjectMocks
-  private WelcomeController underTest;
+  private static final String VIEW_PREFIX = "/WEB-INF/pages/";
+  private static final String VIEW_SUFFIX = ".jsp";
 
-  @BeforeMethod
+  private static final String VIEW_NAME = "candidates";
+
+  private static final String REQUEST_URL = "/secure/welcome";
+
+  private MockMvc mockMvc;
+
+  @Before
   public void setUp() {
-    MockitoAnnotations.initMocks(this);
+    mockMvc =
+        MockMvcBuilders.standaloneSetup(new WelcomeController())
+            .setViewResolvers(viewResolver()).build();
   }
 
   @Test
-  public void shouldReturnViewModel() {
-    //when
-    ModelAndView model = underTest.loadPage();
-    //then
-    assertThat(model.getViewName(), is("candidates"));
+  public void loadPageShouldRenderCandidatesView() throws Exception {
+    mockMvc.perform(get(REQUEST_URL))
+        .andExpect(status().isOk())
+        .andExpect(view().name(VIEW_NAME))
+        .andExpect(forwardedUrl(VIEW_PREFIX + VIEW_NAME + VIEW_SUFFIX));
+  }
+
+
+  private ViewResolver viewResolver() {
+    InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+
+    viewResolver.setPrefix(VIEW_PREFIX);
+    viewResolver.setSuffix(VIEW_SUFFIX);
+
+    return viewResolver;
   }
 }
