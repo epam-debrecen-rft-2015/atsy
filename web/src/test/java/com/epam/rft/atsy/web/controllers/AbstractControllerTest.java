196ddcceed650eea6123cfb03d41c9195a022040
Attila Bagossy <bagossyattila@outlook.com>
2016-07-27 13:49:45 +0200

    Added tests for ChannelController.saveOrUpdate(). Fixed the content-type/encoding of the UncheckedErrorResolver.

--- web/src/test/java/com/epam/rft/atsy/web/controllers/AbstractControllerTest.java
+++ web/src/test/java/com/epam/rft/atsy/web/controllers/AbstractControllerTest.java
@@ -1,16 +1,23 @@
 package com.epam.rft.atsy.web.controllers;
 
+import com.epam.rft.atsy.web.MediaTypes;
+import com.epam.rft.atsy.web.exceptionhandling.GlobalControllerExceptionHandler;
+import com.epam.rft.atsy.web.exceptionhandling.UncheckedExceptionResolver;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 public abstract class AbstractControllerTest {
   protected static final String VIEW_PREFIX = "/WEB-INF/pages/";
   protected static final String VIEW_SUFFIX = ".jsp";
 
   protected MockMvc mockMvc;
+
+  protected ObjectMapper objectMapper;
 
   protected ViewResolver viewResolver() {
     InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -23,9 +30,22 @@ public abstract class AbstractControllerTest {
 
   @Before
   public void setUp() {
+    objectMapper = new ObjectMapper();
+
     mockMvc =
-        MockMvcBuilders.standaloneSetup(controllersUnderTest()).setViewResolvers(viewResolver())
+        MockMvcBuilders.standaloneSetup(controllersUnderTest())
+            .setViewResolvers(viewResolver())
+            .setControllerAdvice(new GlobalControllerExceptionHandler())
+            .setHandlerExceptionResolvers(uncheckedExceptionResolver())
             .build();
+  }
+
+  protected UncheckedExceptionResolver uncheckedExceptionResolver() {
+    MappingJackson2JsonView jsonView = new MappingJackson2JsonView(objectMapper);
+
+    jsonView.setContentType(MediaTypes.APPLICATION_JSON_UTF8.toString());
+
+    return new UncheckedExceptionResolver(jsonView);
   }
 
   protected abstract Object[] controllersUnderTest();
--- web/src/test/java/com/epam/rft/atsy/web/controllers/rest/ChannelControllerTest.java
+++ web/src/test/java/com/epam/rft/atsy/web/controllers/rest/ChannelControllerTest.java
@@ -1,9 +1,17 @@
 package com.epam.rft.atsy.web.controllers.rest;
 
 import static com.jayway.jsonassert.impl.matcher.IsCollectionWithSize.hasSize;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.isNull;
+import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -12,6 +20,8 @@ import com.epam.rft.atsy.service.ChannelService;
 import com.epam.rft.atsy.service.domain.ChannelDTO;
 import com.epam.rft.atsy.web.MediaTypes;
 import com.epam.rft.atsy.web.controllers.AbstractControllerTest;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,6 +32,7 @@ import org.springframework.context.MessageSource;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ChannelControllerTest extends AbstractControllerTest {
@@ -30,6 +41,12 @@ public class ChannelControllerTest extends AbstractControllerTest {
   private static final Long CHANNEL_ID = 1L;
 
   private static final String CHANNEL_NAME = "Channel";
+
+  private static final String REQUEST_BODY_IS_MISSING_MESSAGE = "Required request body is missing";
+
+  private static final String EMPTY_POSITION_NAME_MESSAGE_KEY = "settings.channels.error.empty";
+
+  private static final String VALIDATION_ERROR_MESSAGE = "Validation error";
 
   @Mock
   private ChannelService channelService;
@@ -40,11 +57,19 @@ public class ChannelControllerTest extends AbstractControllerTest {
   @InjectMocks
   private ChannelController channelController;
 
-  private ChannelDTO dummyDto;
+  private ChannelDTO persistedDto;
+
+  private ChannelDTO emptyPostedDto;
+
+  private ChannelDTO correctPostedDto;
 
   @Before
   public void setUpTestData() {
-    dummyDto = ChannelDTO.builder().id(CHANNEL_ID).name(CHANNEL_NAME).build();
+    persistedDto = ChannelDTO.builder().id(CHANNEL_ID).name(CHANNEL_NAME).build();
+
+    emptyPostedDto = ChannelDTO.builder().name(StringUtils.EMPTY).build();
+
+    correctPostedDto = ChannelDTO.builder().name(CHANNEL_NAME).build();
   }
 
   @Override
@@ -53,7 +78,7 @@ public class ChannelControllerTest extends AbstractControllerTest {
   }
 
   @Test
-  public void getChannelsShouldReturnEmptyJsonArrayWhenThereAreNoChannels() throws Exception {
+  public void getChannelsShouldRespondWithEmptyJsonArrayWhenThereAreNoChannels() throws Exception {
     given(channelService.getAllChannels()).willReturn(Collections.emptyList());
 
     mockMvc.perform(get(REQUEST_URL))
@@ -61,11 +86,14 @@ public class ChannelControllerTest extends AbstractControllerTest {
         .andExpect(content().contentType(MediaTypes.APPLICATION_JSON_UTF8))
         .andExpect(jsonPath("$").isArray())
         .andExpect(jsonPath("$").isEmpty());
+
+    then(channelService).should().getAllChannels();
   }
 
   @Test
-  public void getChannelsShouldReturnJsonWithOneChannelWhenThereIsOneChannel() throws Exception {
-    List<ChannelDTO> channels = Collections.singletonList(dummyDto);
+  public void getChannelsShouldRespondWithJsonWithOneChannelWhenThereIsOneChannel()
+      throws Exception {
+    List<ChannelDTO> channels = Collections.singletonList(persistedDto);
 
     given(channelService.getAllChannels()).willReturn(channels);
 
@@ -76,11 +104,14 @@ public class ChannelControllerTest extends AbstractControllerTest {
         .andExpect(jsonPath("$", hasSize(1)))
         .andExpect(jsonPath("$[0].id", is(CHANNEL_ID.intValue())))
         .andExpect(jsonPath("$[0].name", is(CHANNEL_NAME)));
+
+    then(channelService).should().getAllChannels();
   }
 
   @Test
-  public void getChannelsShouldReturnJsonWithThreeChannelsWhenThereAreThreeChannels() throws Exception {
-    List<ChannelDTO> channels = Collections.nCopies(3, dummyDto);
+  public void getChannelsShouldRespondWithJsonWithThreeChannelsWhenThereAreThreeChannels()
+      throws Exception {
+    List<ChannelDTO> channels = Collections.nCopies(3, persistedDto);
 
     given(channelService.getAllChannels()).willReturn(channels);
 
@@ -95,7 +126,60 @@ public class ChannelControllerTest extends AbstractControllerTest {
         .andExpect(jsonPath("$[1].name", is(CHANNEL_NAME)))
         .andExpect(jsonPath("$[2].id", is(CHANNEL_ID.intValue())))
         .andExpect(jsonPath("$[2].name", is(CHANNEL_NAME)));
+
+    then(channelService).should().getAllChannels();
   }
 
+  @Test
+  public void saveOrUpdateShouldRespondWithErrorResponseWhenRequestBodyIsMissing()
+      throws Exception {
+    mockMvc.perform(post(REQUEST_URL)
+        .contentType(MediaTypes.APPLICATION_JSON_UTF8)
+        .accept(MediaTypes.APPLICATION_JSON_UTF8))
+        .andExpect(status().isInternalServerError())
+        .andExpect(content().contentType(MediaTypes.APPLICATION_JSON_UTF8))
+        .andExpect(jsonPath("$.errorMessage").isNotEmpty())
+        .andExpect(jsonPath("$.errorMessage", containsString(REQUEST_BODY_IS_MISSING_MESSAGE)))
+        .andExpect(jsonPath("$.fields").isEmpty());
+  }
 
+  @Test
+  public void saveOrUpdateShouldRespondWithValidationErrorWhenNameIsTheEmptyString()
+      throws Exception {
+    // the value of the id field in the DTO is null so tell the objectMapper
+    // not to include null fields in the JSON
+    objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+    given(
+        messageSource.getMessage(eq(EMPTY_POSITION_NAME_MESSAGE_KEY), isNull(Object[].class),
+            any(Locale.class)))
+        .willReturn(VALIDATION_ERROR_MESSAGE);
+
+    mockMvc.perform(post(REQUEST_URL)
+        .contentType(MediaTypes.APPLICATION_JSON_UTF8)
+        .accept(MediaTypes.APPLICATION_JSON_UTF8)
+        .content(objectMapper.writeValueAsBytes(emptyPostedDto)))
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("$.errorMessage").isNotEmpty())
+        .andExpect(jsonPath("$.errorMessage", equalTo(VALIDATION_ERROR_MESSAGE)))
+        .andExpect(jsonPath("$.fields").isEmpty());
+
+    verifyZeroInteractions(channelService);
+  }
+
+  @Test
+  public void saveOrUpdateShouldRespondWithNoErrorResponseWhenThePostedJsonIsCorrect()
+      throws Exception {
+    mockMvc.perform(post(REQUEST_URL)
+        .contentType(MediaTypes.APPLICATION_JSON_UTF8)
+        .accept(MediaTypes.APPLICATION_JSON_UTF8)
+        .content(objectMapper.writeValueAsBytes(correctPostedDto)))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.errorMessage").isEmpty())
+        .andExpect(jsonPath("$.fields").isEmpty());
+
+    then(channelService).should().saveOrUpdate(correctPostedDto);
+
+    verifyZeroInteractions(messageSource);
+  }
 }
