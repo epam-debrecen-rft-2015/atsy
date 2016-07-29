ff482e1fb23d5bd33b8863f17b49e5dba80b008d
Attila Bagossy <bagossyattila@outlook.com>
2016-07-29 10:50:56 +0200

    Recovered test method for loadPage().

--- web/src/test/java/com/epam/rft/atsy/web/controllers/PasswordChangeControllerTest.java
+++ web/src/test/java/com/epam/rft/atsy/web/controllers/PasswordChangeControllerTest.java
@@ -1,8 +1,14 @@
 package com.epam.rft.atsy.web.controllers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 import com.epam.rft.atsy.service.PasswordChangeService;
 import com.epam.rft.atsy.service.UserService;
 import com.epam.rft.atsy.service.passwordchange.validation.PasswordValidator;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -10,6 +16,10 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PasswordChangeControllerTest extends AbstractControllerTest {
+  private static final String REQUEST_URL = "/secure/password/manage";
+
+  private static final String VIEW_NAME = "password_change";
+
   @Mock
   private PasswordChangeService passwordChangeService;
 
@@ -25,5 +35,13 @@ public class PasswordChangeControllerTest extends AbstractControllerTest {
   @Override
   protected Object[] controllersUnderTest() {
     return new Object[]{passwordChangeController};
+  }
+
+  @Test
+  public void loadPageShouldRenderPasswordChangeView() throws Exception {
+    mockMvc.perform(get(REQUEST_URL))
+        .andExpect(status().isOk())
+        .andExpect(view().name(VIEW_NAME))
+        .andExpect(forwardedUrl(VIEW_PREFIX + VIEW_NAME + VIEW_SUFFIX));
   }
 }
