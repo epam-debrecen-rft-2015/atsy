d47e8573666e10539d67dc6e874d8509c0923c2e
Attila Bagossy <bagossyattila@outlook.com>
2016-08-05 11:22:59 +0200

    Added some documentation to the MessageKeyResolverImpl class.

--- web/src/main/java/com/epam/rft/atsy/web/messageresolution/MessageKeyResolverImpl.java
+++ web/src/main/java/com/epam/rft/atsy/web/messageresolution/MessageKeyResolverImpl.java
@@ -7,9 +7,21 @@ import org.springframework.context.i18n.LocaleContextHolder;
 
 import java.util.Locale;
 
+/**
+ * Provides a developer friendly way of message key resolution by implementing the {@code
+ * MessageKeyResolver} interface. Also implements the {@code MessageSource} interface so can be used
+ * as the default {@code MessageSource}. Behaves as a proxy on top of an underlying {@code
+ * MessageSource} implementation and delegates the methods inherited from the {@code MessageSource}
+ * interface to the underlying concrete {@code MessageSource} object.
+ */
 public class MessageKeyResolverImpl implements MessageKeyResolver, MessageSource {
   private MessageSource messageSource;
 
+  /**
+   * Constructs a new {@code MessageKeyResolverImpl} instance that will act as a proxy on top of the
+   * passed {@code MessageSource}.
+   * @param underlyingMessageSource the {@code MessageSource} to hide
+   */
   public MessageKeyResolverImpl(MessageSource underlyingMessageSource) {
     this.messageSource = underlyingMessageSource;
   }
