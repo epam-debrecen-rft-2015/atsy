ced79d70ac1213f2c577f986a6eca6421c359b3a
Attila Bagossy <bagossyattila@outlook.com>
2016-08-16 14:20:11 +0200

    Replaced MessageSource in controller and test classes.

--- web/src/main/java/com/epam/rft/atsy/web/controllers/ApplicationStateController.java
+++ web/src/main/java/com/epam/rft/atsy/web/controllers/ApplicationStateController.java
@@ -6,9 +6,9 @@ import com.epam.rft.atsy.service.StatesHistoryService;
 import com.epam.rft.atsy.service.domain.states.StateDTO;
 import com.epam.rft.atsy.service.domain.states.StateHistoryDTO;
 import com.epam.rft.atsy.web.StateHistoryViewRepresentation;
+import com.epam.rft.atsy.web.messageresolution.MessageKeyResolver;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,7 +50,7 @@ public class ApplicationStateController {
   private StateService stateService;
 
   @Resource
-  private MessageSource messageSource;
+  private MessageKeyResolver messageKeyResolver;
 
   @Resource
   private ModelMapper modelMapper;
@@ -79,9 +79,10 @@ public class ApplicationStateController {
     for (StateHistoryViewRepresentation stateHistoryViewRepresentation : stateHistoryViewRepresentations) {
       String stateType = stateHistoryViewRepresentation.getStateName();
       stateType =
-          messageSource
-              .getMessage(APPLICATION_STATE + stateHistoryViewRepresentation.getStateName(),
-                  new Object[]{stateType}, locale);
+          messageKeyResolver
+              .resolveMessageOrDefault(
+                  APPLICATION_STATE + stateHistoryViewRepresentation.getStateName(),
+                  stateType);
       stateHistoryViewRepresentation.setStateFullName(stateType);
     }
 
