e4e0f8bd8fcad1e87aba7ec5a81f3d19edcf271c
Iványi-Nagy Gábor <gabilampard@gmail.com>
2016-08-18 01:20:29 +0200

    Added FileIsMissingValidationException Added regular expression (file format)

--- web/src/main/java/com/epam/rft/atsy/web/mapper/FileValidationRule.java
+++ web/src/main/java/com/epam/rft/atsy/web/mapper/FileValidationRule.java
@@ -3,7 +3,8 @@ package com.epam.rft.atsy.web.mapper;
 
 import com.epam.rft.atsy.service.exception.file.FileIsAlreadyExistValidationException;
 import com.epam.rft.atsy.service.exception.file.FileIsEmptyValidationException;
-import com.epam.rft.atsy.service.exception.file.FileIsInWrongExtensionValidationException;
+import com.epam.rft.atsy.service.exception.file.FileIsInWrongSyntaxValidationException;
+import com.epam.rft.atsy.service.exception.file.FileIsMissingValidationException;
 import com.epam.rft.atsy.service.exception.file.FileIsTooLargeValidationException;
 
 import lombok.AllArgsConstructor;
@@ -13,9 +14,10 @@ import lombok.Getter;
 @Getter
 public enum FileValidationRule {
 
-  FILE_IS_TOO_LARGE("file.is.too.large", FileIsTooLargeValidationException.class),
+  FILE_IS_MISSING("file.is.missing", FileIsMissingValidationException.class),
   FILE_IS_EMPTY("file.is.empty", FileIsEmptyValidationException.class),
-  FILE_IS_IN_WRONG_EXTENSION("file.is.in.wrong.extension", FileIsInWrongExtensionValidationException.class),
+  FILE_IS_TOO_LARGE("file.is.too.large", FileIsTooLargeValidationException.class),
+  FILE_IS_IN_WRONG_EXTENSION("file.is.in.wrong.syntax", FileIsInWrongSyntaxValidationException.class),
   FILE_IS_ALREADY_EXIST("file.is.already.exist", FileIsAlreadyExistValidationException.class);
 
   private final String messageKey;
--- web/src/main/java/com/epam/rft/atsy/web/validator/FileValidatorImpl.java
+++ web/src/main/java/com/epam/rft/atsy/web/validator/FileValidatorImpl.java
@@ -2,7 +2,8 @@ package com.epam.rft.atsy.web.validator;
 
 
 import com.epam.rft.atsy.service.exception.file.FileIsEmptyValidationException;
-import com.epam.rft.atsy.service.exception.file.FileIsInWrongExtensionValidationException;
+import com.epam.rft.atsy.service.exception.file.FileIsInWrongSyntaxValidationException;
+import com.epam.rft.atsy.service.exception.file.FileIsMissingValidationException;
 import com.epam.rft.atsy.service.exception.file.FileIsTooLargeValidationException;
 import com.epam.rft.atsy.service.exception.file.FileValidationException;
 
@@ -17,7 +18,8 @@ import java.util.regex.Pattern;
 public class FileValidatorImpl implements FileValidator {
 
   private static final long MAX_FILE_SIZE = 1024 * 1024 * 10; // 10 MB
-  private static final String FILE_PATTERN = "([^\\s]+(\\.(?i)(doc|docx|odt|pdf))$)";
+  // Example: Garfield_Hungry-Angry_2010-12-24.doc
+  private static final String FILE_PATTERN = "([A-Z][a-z]+[-|_])+[0-9-]+[.](doc|docx|odt|pdf)$";
 
   private Pattern pattern;
   private Matcher matcher;
@@ -26,17 +28,23 @@ public class FileValidatorImpl implements FileValidator {
   public void validate(MultipartFile file) throws FileValidationException {
     Assert.notNull(file);
 
+    if (isMissing(file)) {
+      throw new FileIsMissingValidationException();
+    }
     if (isEmptyFile(file)) {
       throw new FileIsEmptyValidationException();
     }
     if (isGreaterFileSizeThanTheMaximumFileSize(file)) {
       throw new FileIsTooLargeValidationException();
     }
-    /*if (isUnCorrectFileExtension(file)) {
-      throw new FileIsInWrongExtensionValidationException();
-    }*/
+    if (isFileWithoutTheCorrectSyntax(file)) {
+      throw new FileIsInWrongSyntaxValidationException();
+    }
   }
 
+  protected boolean isMissing(MultipartFile file) {
+    return file.getOriginalFilename().isEmpty();
+  }
 
   protected boolean isEmptyFile(MultipartFile file) {
     return file.isEmpty();
@@ -46,14 +54,13 @@ public class FileValidatorImpl implements FileValidator {
     return file.getSize() > MAX_FILE_SIZE;
   }
 
-  protected boolean isUnCorrectFileExtension(MultipartFile file) {
-    return !isCorrectFileExtension(file);
+  protected boolean isFileWithoutTheCorrectSyntax(MultipartFile file) {
+    return !isFileWithTheCorrectSyntax(file);
   }
 
-  protected boolean isCorrectFileExtension(MultipartFile file) {
+  protected boolean isFileWithTheCorrectSyntax(MultipartFile file) {
     pattern = Pattern.compile(FILE_PATTERN);
     matcher = pattern.matcher(file.getOriginalFilename());
-    System.out.println(file.getOriginalFilename());
     return matcher.matches();
   }
 }
