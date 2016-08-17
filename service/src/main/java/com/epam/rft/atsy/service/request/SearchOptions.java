49fa9bff7ec0d5b67c7cc7814dd7d59d86dc2b50
domokoslaci94 <domokoslaci94@gmail.com>
2016-08-17 10:35:03 +0200

    Added java documentation.

--- service/src/main/java/com/epam/rft/atsy/service/request/SearchOptions.java
+++ service/src/main/java/com/epam/rft/atsy/service/request/SearchOptions.java
@@ -9,17 +9,27 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
+/**
+ * Represent a search. It stores the values of the searchable fields
+ */
 @Builder
 @Data
 @NoArgsConstructor
 public class SearchOptions {
 
-
   private String name;
   private String email;
   private String phone;
 
-
+  /**
+   * Constructs a new instance of {@code SearchOptions}.
+   * @param name a String which represents the value of the name field which will be searched in the
+   * database
+   * @param email a String which represents the value of the email field which will be searched in
+   * the database
+   * @param phone a String which represents the value of the phone field which will be searched in
+   * the database
+   */
   public SearchOptions(String name, String email, String phone) {
     this.name = MoreObjects.firstNonNull(name, StringUtils.EMPTY);
     this.email = MoreObjects.firstNonNull(email, StringUtils.EMPTY);
