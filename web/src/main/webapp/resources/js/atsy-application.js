9eeb900891dc809fbda762c5570dda8febcebdfc
domokoslaci94 <domokoslaci94@gmail.com>
2016-08-22 14:50:00 +0200

    Minor fixes.

--- web/src/main/webapp/resources/js/atsy-application.js
+++ web/src/main/webapp/resources/js/atsy-application.js
@@ -14,6 +14,6 @@ $.getJSON('/atsy/secure/positions', { get_param: 'value' }, function(data) {
 
 $.getJSON('/atsy/secure/channels', { get_param: 'value' }, function(data) {
     $.each(data, function(index, element) {
-        $('#position').append($('<option value="'+element.id+'"></option>').text(element.name));
+        $('#channel').append($('<option value="'+element.id+'"></option>').text(element.name));
     });
 });
