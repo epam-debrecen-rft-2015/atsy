05797ab9f77234294145cefd9cf2588e6d5cb2b0
Iványi-Nagy Gábor <gabilampard@gmail.com>
2016-08-25 08:29:17 +0200

    Added null test case for FileValidator validate() method

--- web/src/test/java/com/epam/rft/atsy/web/validator/FileValidatorImplTest.java
+++ web/src/test/java/com/epam/rft/atsy/web/validator/FileValidatorImplTest.java
@@ -35,6 +35,13 @@ public class FileValidatorImplTest {
 
   private FileValidator fileValidator = new FileValidatorImpl();
 
+
+  @Test(expected = IllegalArgumentException.class)
+  public void validateShouldThrowIllegalArgumentExceptionWhenFileIsNull() throws FileValidationException {
+
+    fileValidator.validate(null);
+  }
+
   @Test(expected = FileIsMissingValidationException.class)
   public void isMissingFileShouldThrowFileIsMissingValidationExceptionWhenOriginalFilenameIsEmpty()
       throws FileValidationException {
