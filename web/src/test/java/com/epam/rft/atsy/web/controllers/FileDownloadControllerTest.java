8e95f646010ac0b8c382f87d107ec87e73055ac8
Iványi-Nagy Gábor <gabilampard@gmail.com>
2016-08-28 20:04:11 +0200

    Finish FileDownloadControllerTest.java

--- web/src/test/java/com/epam/rft/atsy/web/controllers/FileDownloadControllerTest.java
+++ web/src/test/java/com/epam/rft/atsy/web/controllers/FileDownloadControllerTest.java
@@ -1,6 +1,7 @@
 package com.epam.rft.atsy.web.controllers;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -10,9 +11,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.epam.rft.atsy.service.CandidateService;
 import com.epam.rft.atsy.service.domain.CandidateDTO;
+import com.epam.rft.atsy.web.util.CandidateCVFileHandlerUtil;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -22,7 +23,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.util.FileCopyUtils;
 
 import java.io.File;
 import java.io.IOException;
@@ -43,10 +47,8 @@ public class FileDownloadControllerTest extends AbstractControllerTest {
   private static final String CATALINA_BASE = "${catalina.base}";
   private static final String CV_TEST_FOLDER_NAME = "cv_test_folder";
 
-  private static final String CV_TEST_FOLDER_LOCATION_PATH =
-      System.getProperty(CATALINA_BASE) + File.separator + CV_TEST_FOLDER_NAME;
-  private static final File FOLDER = new File(CV_TEST_FOLDER_LOCATION_PATH);
-
+  private static final String CV_TEST_FOLDER_LOCATION_PATH = System.getProperty(CATALINA_BASE) + File.separator + CV_TEST_FOLDER_NAME;
+  private static final File TEST_FOLDER = new File(CV_TEST_FOLDER_LOCATION_PATH);
 
   private static final String CANDIDATE_ID_PARAM_NAME = "candidateId";
   private static final String CANDIDATE_ID_PARAM_VALUE = "1L";
@@ -54,13 +56,12 @@ public class FileDownloadControllerTest extends AbstractControllerTest {
   private static final long CANDIDATE_ID = 1L;
   private static final String CANDIDATE_NAME = "Candidate A";
   private static final String CANDIDATE_CV_FILENAME = "file.pdf";
-  private static final long FILE_SIZE_HUNDRED_BYTE = 100;
+  private static final int FILE_SIZE_HUNDRED_BYTE = 100;
+  private static final char CHARACTER = 'a';
 
-  private CandidateDTO candidateDTOWithoutCVFile =
-      CandidateDTO.builder().id(CANDIDATE_ID).name(CANDIDATE_NAME).cvFilename(null).build();
-  private CandidateDTO candidateDTOWithCVFile =
-      CandidateDTO.builder().id(CANDIDATE_ID).name(CANDIDATE_NAME).cvFilename(CANDIDATE_CV_FILENAME)
-          .build();
+  private CandidateDTO candidateDTOWitNullCVFilename = CandidateDTO.builder().id(CANDIDATE_ID).name(CANDIDATE_NAME).cvFilename(null).build();
+  private CandidateDTO candidateDTOWithEmptyCVFilename = CandidateDTO.builder().id(CANDIDATE_ID).name(CANDIDATE_NAME).cvFilename(StringUtils.EMPTY).build();
+  private CandidateDTO candidateDTOWithValidCVFilename = CandidateDTO.builder().id(CANDIDATE_ID).name(CANDIDATE_NAME).cvFilename(CANDIDATE_CV_FILENAME).build();
 
   @Mock
   private CandidateService candidateService;
@@ -76,47 +77,61 @@ public class FileDownloadControllerTest extends AbstractControllerTest {
 
   @BeforeClass
   public static void doBeforeClass() throws IOException {
-    FileUtils.forceMkdir(FOLDER);
+    CandidateDTO candidateDTO = CandidateDTO.builder().id(CANDIDATE_ID).name(CANDIDATE_NAME).cvFilename(null).build();
+
+    FileUtils.forceMkdir(TEST_FOLDER);
+    CandidateCVFileHandlerUtil.createCandidateFolderOnFolderLocation(CV_TEST_FOLDER_LOCATION_PATH, candidateDTO);
+    File file = CandidateCVFileHandlerUtil.createCVFileFromFolderLocationAndCandidateDtoAndCVFilename(CV_TEST_FOLDER_LOCATION_PATH, candidateDTO, CANDIDATE_CV_FILENAME);
+    FileCopyUtils.copy(StringUtils.repeat(CHARACTER, FILE_SIZE_HUNDRED_BYTE).getBytes(), file);
   }
 
   @Before
   public void setup() {
-    ReflectionTestUtils.setField(fileDownloadController, UPLOAD_LOCATION_VARIABLE_NAME,
-        CV_TEST_FOLDER_LOCATION_PATH);
-  }
-
-  @After
-  public void tearDown() throws IOException {
-    FileUtils.cleanDirectory(FOLDER);
+    ReflectionTestUtils.setField(fileDownloadController, UPLOAD_LOCATION_VARIABLE_NAME, CV_TEST_FOLDER_LOCATION_PATH);
   }
 
   @AfterClass
   public static void doAfterClass() throws IOException {
-    FileUtils.forceDelete(FOLDER);
+    FileUtils.forceDelete(TEST_FOLDER);
   }
 
   @Test
   public void downloadFileShouldNotDownloadWhenTheNameOfTheCVFileIsNullInDB() throws Exception {
-    given(candidateService.getCvFilenameById(CANDIDATE_ID)).willReturn(null);
+    given(candidateService.getCandidate(CANDIDATE_ID)).willReturn(candidateDTOWitNullCVFilename);
     this.mockMvc.perform(get(REQUEST_URL).param(CANDIDATE_ID_PARAM_NAME, CANDIDATE_ID_PARAM_VALUE))
         .andExpect(status().is3xxRedirection())
-        .andExpect(redirectedUrl(REDIRECT_URL))
         .andExpect(flash().attributeExists(FILE_ERROR_MESSAGE))
-        .andExpect(flash().attribute(FILE_ERROR_MESSAGE, equalTo(FILE_ERROR_MESSAGE_KEY)));
+        .andExpect(flash().attribute(FILE_ERROR_MESSAGE, equalTo(FILE_ERROR_MESSAGE_KEY)))
+        .andExpect(redirectedUrl(REDIRECT_URL));
 
-    then(candidateService).should().getCvFilenameById(CANDIDATE_ID);
+    then(candidateService).should().getCandidate(CANDIDATE_ID);
   }
 
   @Test
   public void downloadFileShouldNotDownloadWhenTheNameOfTheCVFileIsEmptyInDB() throws Exception {
-    given(candidateService.getCvFilenameById(CANDIDATE_ID)).willReturn(StringUtils.EMPTY);
+    given(candidateService.getCandidate(CANDIDATE_ID)).willReturn(candidateDTOWithEmptyCVFilename);
+
     this.mockMvc.perform(get(REQUEST_URL).param(CANDIDATE_ID_PARAM_NAME, CANDIDATE_ID_PARAM_VALUE))
         .andExpect(status().is3xxRedirection())
-        .andExpect(redirectedUrl(REDIRECT_URL))
         .andExpect(flash().attributeExists(FILE_ERROR_MESSAGE))
-        .andExpect(flash().attribute(FILE_ERROR_MESSAGE, equalTo(FILE_ERROR_MESSAGE_KEY)));
+        .andExpect(flash().attribute(FILE_ERROR_MESSAGE, equalTo(FILE_ERROR_MESSAGE_KEY)))
+        .andExpect(redirectedUrl(REDIRECT_URL));
 
-    then(candidateService).should().getCvFilenameById(CANDIDATE_ID);
+    then(candidateService).should().getCandidate(CANDIDATE_ID);
   }
 
+  @Test
+  public void downloadFileShouldDownloadWhenTheNameOfTheCVFileExistsInDB() throws Exception {
+    given(candidateService.getCandidate(CANDIDATE_ID)).willReturn(candidateDTOWithValidCVFilename);
+
+    MockHttpServletResponse result = this.mockMvc.perform(get(REQUEST_URL).param(CANDIDATE_ID_PARAM_NAME, CANDIDATE_ID_PARAM_VALUE))
+        .andExpect(status().isOk())
+        .andReturn().getResponse();
+
+    assertThat(INLINE_FORMAT + CANDIDATE_CV_FILENAME, equalTo(result.getHeader(CONTENT_DISPOSITION)));
+    assertThat(MediaType.APPLICATION_OCTET_STREAM_VALUE, equalTo(result.getContentType()));
+    assertThat(result.getContentLength(), equalTo(FILE_SIZE_HUNDRED_BYTE));
+
+    then(candidateService).should().getCandidate(CANDIDATE_ID);
+  }
 }
