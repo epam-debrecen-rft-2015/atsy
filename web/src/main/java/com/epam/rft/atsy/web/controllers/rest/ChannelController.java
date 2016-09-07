1036a466cf4eff2837b9afcf00f0531b62c70957
Iványi-Nagy Gábor <gabilampard@gmail.com>
2016-09-08 00:47:33 +0200

    Added deleteChannelDtoLogicallyByName() method to ChannelController

--- web/src/main/java/com/epam/rft/atsy/web/controllers/rest/ChannelController.java
+++ web/src/main/java/com/epam/rft/atsy/web/controllers/rest/ChannelController.java
@@ -2,16 +2,16 @@ package com.epam.rft.atsy.web.controllers.rest;
 
 import com.epam.rft.atsy.service.ChannelService;
 import com.epam.rft.atsy.service.domain.ChannelDTO;
+import com.epam.rft.atsy.service.exception.ChannelNotFoundException;
 import com.epam.rft.atsy.web.exceptionhandling.RestResponse;
 import com.epam.rft.atsy.web.messageresolution.MessageKeyResolver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
@@ -23,11 +23,10 @@ import javax.validation.Valid;
  * REST controller used to retrieve or modify information about the stored channels
  */
 @RestController
-@RequestMapping(value = "/secure/channels")
+@RequestMapping(path = "/secure/channels")
 public class ChannelController {
   private static final String EMPTY_POSITION_NAME_MESSAGE_KEY = "settings.channels.error.empty";
-  private static final String TECHNICAL_ERROR_MESSAGE_KEY = "technical.error.message";
-  private static final Logger LOGGER = LoggerFactory.getLogger(ChannelController.class);
+  private static final String SELECTED_CHANNEL_NOT_FOUND_MESSAGE_KEY = "selected.channel.not.found";
 
   @Resource
   private ChannelService channelService;
@@ -65,6 +64,20 @@ public class ChannelController {
 
       RestResponse restResponse = new RestResponse(errorMessage);
 
+      return new ResponseEntity<>(restResponse, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @RequestMapping(path = "/delete", method = RequestMethod.POST)
+  public ResponseEntity<RestResponse> deleteChannelDtoLogicallyByName(@RequestParam(name = "channelName") String channelName) {
+
+    try {
+      channelService.deleteChannelDtoLogicallyByName(channelName);
+      return new ResponseEntity<>(RestResponse.NO_ERROR, HttpStatus.OK);
+
+    } catch (ChannelNotFoundException e) {
+      String errorMessage = messageKeyResolver.resolveMessageOrDefault(SELECTED_CHANNEL_NOT_FOUND_MESSAGE_KEY);
+      RestResponse restResponse = new RestResponse(errorMessage);
       return new ResponseEntity<>(restResponse, HttpStatus.BAD_REQUEST);
     }
   }
