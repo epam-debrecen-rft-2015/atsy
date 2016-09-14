d1cc049d6c0ea44558a808f3c125deb02674c4c9
Iványi-Nagy Gábor <gabilampard@gmail.com>
2016-09-14 15:30:40 +0200

    Added deletePositionDtoLogicallyByName() method Added test cases

--- web/src/main/java/com/epam/rft/atsy/web/controllers/rest/PositionController.java
+++ web/src/main/java/com/epam/rft/atsy/web/controllers/rest/PositionController.java
@@ -2,20 +2,22 @@ package com.epam.rft.atsy.web.controllers.rest;
 
 import com.epam.rft.atsy.service.PositionService;
 import com.epam.rft.atsy.service.domain.PositionDTO;
+import com.epam.rft.atsy.service.exception.PositionNotFoundException;
 import com.epam.rft.atsy.web.exceptionhandling.RestResponse;
 import com.epam.rft.atsy.web.messageresolution.MessageKeyResolver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 import java.util.Locale;
+
 import javax.annotation.Resource;
 import javax.validation.Valid;
 
@@ -26,8 +28,7 @@ import javax.validation.Valid;
 @RequestMapping(value = "/secure/positions")
 public class PositionController {
   private static final String EMPTY_POSITION_NAME_MESSAGE_KEY = "settings.positions.error.empty";
-  private static final String TECHNICAL_ERROR_MESSAGE_KEY = "technical.error.message";
-  private static final Logger LOGGER = LoggerFactory.getLogger(PositionController.class);
+  private static final String SELECTED_POSITION_NOT_FOUND_MESSAGE_KEY = "selected.position.not.found.js";
 
   @Resource
   private PositionService positionService;
@@ -36,18 +37,20 @@ public class PositionController {
 
   /**
    * Get all the available positions.
+   *
    * @return a collection of the available positions wrapped in PositionDTO objects
    */
   @RequestMapping(method = RequestMethod.GET)
   public Collection<PositionDTO> getPositions() {
-    return positionService.getAllPositions();
+    return positionService.getAllNonDeletedPositionDto();
   }
 
   /**
    * Saves or updates and existing position.
+   *
    * @param positionDTO an object which wraps the data of a position
-   * @param result an object used to check if any error occurs
-   * @param locale language of the response
+   * @param result      an object used to check if any error occurs
+   * @param locale      language of the response
    * @return a ResponseEntity object, which contains HTTP status code and error message if it occurs
    */
   @RequestMapping(method = RequestMethod.POST)
@@ -58,11 +61,25 @@ public class PositionController {
 
       return new ResponseEntity<>(RestResponse.NO_ERROR, HttpStatus.OK);
     } else {
-      String errorMessage =
-          messageKeyResolver.resolveMessageOrDefault(EMPTY_POSITION_NAME_MESSAGE_KEY);
+      String errorMessage = messageKeyResolver.resolveMessageOrDefault(EMPTY_POSITION_NAME_MESSAGE_KEY);
 
       RestResponse restResponse = new RestResponse(errorMessage);
 
+      return new ResponseEntity<>(restResponse, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @RequestMapping(path = "/delete", method = RequestMethod.DELETE)
+  public ResponseEntity<RestResponse> deletePositionDtoLogicallyByName(@RequestParam(name = "positionId") Long positionId) {
+
+    try {
+      positionService.deletePositionDtoLogicallyById(positionId);
+      return new ResponseEntity<>(RestResponse.NO_ERROR, HttpStatus.OK);
+
+    } catch (PositionNotFoundException e) {
+      String errorMessage = messageKeyResolver.resolveMessageOrDefault(SELECTED_POSITION_NOT_FOUND_MESSAGE_KEY);
+
+      RestResponse restResponse = new RestResponse(errorMessage);
       return new ResponseEntity<>(restResponse, HttpStatus.BAD_REQUEST);
     }
   }
