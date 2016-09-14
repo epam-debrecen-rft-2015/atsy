356e8b83a02c7a4f90caa6b31bd34cb58436475c
Iványi-Nagy Gábor <gabilampard@gmail.com>
2016-09-14 15:13:11 +0200

    Added deletePositionDtoLogicallyById method Modified saveOrUpdate method

---
+++ service/src/main/java/com/epam/rft/atsy/service/exception/PositionNotFoundException.java
@@ -0,0 +1,9 @@
+package com.epam.rft.atsy.service.exception;
+
+/**
+ * This exception indicates when we did not found channel in the database.
+ */
+public class PositionNotFoundException extends Exception {
+
+  public PositionNotFoundException() {}
+}
