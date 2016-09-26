fe8df0a93b78106e144c264c8ada1e657e9862cb
Iványi-Nagy Gábor <gabilampard@gmail.com>
2016-09-26 13:18:49 +0200

    Test: FileUploadController
    Added a new messageKey-exception pair to ExceptionMapperConfiguration

--- service/src/main/java/com/epam/rft/atsy/service/exception/file/CandidateAlreadyHasCVFileException.java
+++ service/src/main/java/com/epam/rft/atsy/service/exception/file/CandidateAlreadyHasCVFileException.java
@@ -1,9 +1,9 @@
-package com.epam.rft.atsy.service.exception;
+package com.epam.rft.atsy.service.exception.file;
 
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-public class CandidateAlreadyHasCVFileException extends Exception {
+public class CandidateAlreadyHasCVFileException extends FileValidationException {
 
   public CandidateAlreadyHasCVFileException(String message) {
     super(message);
