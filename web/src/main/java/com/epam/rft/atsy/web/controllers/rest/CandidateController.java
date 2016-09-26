194a46fdd7fdcd1071da183998da6c2dac170230
domokoslaci94 <domokoslaci94@gmail.com>
2016-09-26 14:24:35 +0200

    Removed getAllNonDeletedDto function from abstract controller.

--- web/src/main/java/com/epam/rft/atsy/web/controllers/rest/CandidateController.java
+++ web/src/main/java/com/epam/rft/atsy/web/controllers/rest/CandidateController.java
@@ -7,17 +7,25 @@ import com.epam.rft.atsy.service.domain.CandidateDTO;
 import com.epam.rft.atsy.service.request.CandidateFilterRequest;
 import com.epam.rft.atsy.service.request.SearchOptions;
 import com.epam.rft.atsy.service.response.PagingResponse;
+import com.epam.rft.atsy.web.controllers.LogicallyDeletableAbstractController;
+import com.epam.rft.atsy.web.exceptionhandling.RestResponse;
+import com.epam.rft.atsy.web.messageresolution.MessageKeyResolver;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import javax.annotation.Resource;
+import java.util.Locale;
+import javax.validation.Valid;
 
 /**
  * REST controller used to retrieve information from the stored candidates in JSON format.
@@ -25,7 +33,7 @@ import javax.annotation.Resource;
  */
 @RestController
 @RequestMapping(value = "/secure/candidates")
-public class CandidateController {
+public class CandidateController extends LogicallyDeletableAbstractController<CandidateDTO> {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(CandidateController.class);
 
@@ -34,8 +42,15 @@ public class CandidateController {
       SearchOptions.builder().name(StringUtils.EMPTY).email(StringUtils.EMPTY)
           .phone(StringUtils.EMPTY).build();
 
-  @Resource
   private CandidateService candidateService;
+
+  @Autowired
+  public CandidateController(
+      CandidateService candidateService,
+      MessageKeyResolver messageKeyResolver) {
+    super(candidateService, messageKeyResolver);
+    this.candidateService = candidateService;
+  }
 
   /**
    * Loads the page according to the given parameters.
@@ -87,5 +102,11 @@ public class CandidateController {
       LOGGER.error("Cannot read filters from json", e);
       return EMPTY_SERCH_OPTIONS;
     }
+  }
+
+  @Override
+  public ResponseEntity<RestResponse> saveOrUpdate(@Valid @RequestBody CandidateDTO dto,
+                                                   BindingResult bindingResult, Locale locale) {
+    return null;
   }
 }
