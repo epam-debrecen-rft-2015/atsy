25acd878330ded6dec9caf696aa27d8a6fd961ac
domokoslaci94 <domokoslaci94@gmail.com>
2016-10-07 13:13:19 +0200

    Modified tests.

--- web/src/test/java/com/epam/rft/atsy/cucumber/util/CommonStepDefs.java
+++ web/src/test/java/com/epam/rft/atsy/cucumber/util/CommonStepDefs.java
@@ -4,6 +4,9 @@ import static com.epam.rft.atsy.cucumber.util.DriverProvider.getDriver;
 import static com.epam.rft.atsy.cucumber.util.DriverProvider.waitForPageLoadAfter;
 
 import org.openqa.selenium.By;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import cucumber.api.java.en.Given;
 
@@ -11,9 +14,16 @@ public class CommonStepDefs {
 
   @Given("The user signed in")
   public void loggedIn() {
-    getDriver().get("http://localhost:8080/atsy/login?locale=hu");
-    getDriver().findElement(By.id("name")).sendKeys("test");
-    getDriver().findElement(By.id("password")).sendKeys("pass3");
-    waitForPageLoadAfter(event -> getDriver().findElement(By.id("loginButton")).click());
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    if (authentication != null && authentication.isAuthenticated()
+        && !(authentication instanceof AnonymousAuthenticationToken)) {
+      getDriver().get("http://localhost:8080/atsy/login?locale=hu");
+      getDriver().findElement(By.id("name")).sendKeys("test");
+      getDriver().findElement(By.id("password")).sendKeys("pass3");
+      waitForPageLoadAfter(event -> getDriver().findElement(By.id("loginButton")).click());
+    } else {
+      getDriver().get("http://localhost:8080/atsy/secure/welcome");
+    }
   }
 }
